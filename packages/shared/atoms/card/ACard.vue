e82acb55d6a6174b8fe67e3d0cfdb88dcbe18f60
kamil_pula <sorennx@gmail.com>
2023-08-06 17:44:29 +0200

    Added ACard component

--- packages/shared/atoms/card/ACard.vue
+++ packages/shared/atoms/card/ACard.vue
@@ -1,27 +1,65 @@
 <template>
-  <div>
-    <div class="test"></div>
+  <div class="a-card">
+    <div class="a-card__header">
+      <img :src="props.headerImage" />
+    </div>
+    <div class="a-card__body">
+      <slot name="body" />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  headerImage: {
+    type: String,
+    default: 'https://www.qualia.com/images/branding/qualia_logo_medium.png'
+  }
+})
+</script>
 
 <style scoped lang="scss">
-.test {
-  min-width: 100px;
-  min-height: 100px;
-  background: #fff;
+.a-card {
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  align-items: flex-start;
 
-  @media (#{map-get($screen-breakpoints, 'sm-and-up')}) {
-    background: red;
+  width: 300px;
+  height: 210px;
+
+  border-radius: $global-border-radius-10;
+  box-shadow: 4px 4px 4px 0 rgb(0 0 0 / 25%);
+
+  &__header {
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    align-self: stretch;
+    justify-content: center;
+
+    height: 120px;
+
+    background: $global-gradients-blue-primary-1;
+    border-radius: $global-border-radius-10 $global-border-radius-10 0 0;
+
+    img {
+      object-fit: scale-down;
+      border-radius: $global-border-radius-10 $global-border-radius-10 0 0;
+    }
   }
 
-  @media (#{map-get($screen-breakpoints, 'md-and-up')}) {
-    background: green;
-  }
+  &__body {
+    display: flex;
+    flex: 1 0 0;
+    flex-direction: column;
+    gap: $global-spacing-20;
+    align-self: stretch;
 
-  @media (#{map-get($screen-breakpoints, 'lg-and-up')}) {
-    background: blue;
+    padding: $global-spacing-20;
+
+    background: $global-colors-white;
+    border-radius: 0 0 $global-border-radius-10 $global-border-radius-10;
   }
 }
 </style>
