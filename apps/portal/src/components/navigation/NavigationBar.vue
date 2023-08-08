868b32a6aa1b4374e8683fb1ba0c80b38b671704
krl8456 <karolrak50@gmail.com>
2023-08-08 21:38:10 +0200

    Add NavigationBarMainSection

--- apps/portal/src/components/navigation/NavigationBar.vue
+++ apps/portal/src/components/navigation/NavigationBar.vue
@@ -1,11 +1,21 @@
 <template>
-  <nav>
+  <nav class="navigation-bar">
     <div>img</div>
-    <div>navigationbarmainitems</div>
+    <navigation-bar-main-section />
     <div>navigationbarauthsection</div>
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavigationBarMainSection from '@/components/navigation/sections/NavigationBarMainSection.vue'
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.navigation-bar {
+  display: flex;
+  gap: 16px;
+  align-items: flex-start;
+  align-self: stretch;
+  padding: 8px 12px;
+}
+</style>
