84a197628f7e305e43fcb77bc37102058b2c1687
kamil_pula <sorennx@gmail.com>
2023-08-11 00:17:43 +0200

    Added translations, style & implemented a-header

--- apps/portal/src/views/Home/components/PartnersSection.vue
+++ apps/portal/src/views/Home/components/PartnersSection.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="partners-section">
-    <div>Header</div>
+    <a-header
+      :title="$t('company.partners.ourPartners')"
+      :subtitle="$t('company.partners.companiesThatChoseUs')"
+    />
     <company-card-list :companies="companies" />
   </div>
 </template>
 
 <script setup lang="ts">
 // Components
+import AHeader from '@sharedAtoms/header/AHeader.vue'
 import CompanyCardList from '@/components/core/company/card/CompanyCardList.vue'
 
 const companies = {}
@@ -14,7 +18,12 @@ const companies = {}
 
 <style scoped lang="scss">
 .partners-section {
+  display: flex;
+  flex-direction: column;
+  gap: $global-spacing-30;
+
   padding: $global-spacing-30 $global-spacing-40;
+
   background-color: $global-colors-white;
 }
 </style>
