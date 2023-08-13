e584db1c39155583782932fb068fc66cd67dcc14
kamil_pula <sorennx@gmail.com>
2023-08-14 01:58:38 +0200

    Update HomeView.vue

--- apps/portal/src/views/Home/HomeView.vue
+++ apps/portal/src/views/Home/HomeView.vue
@@ -8,5 +8,5 @@
 <script setup lang="ts">
 // Components
 import NavigationBar from '@/components/navigation/NavigationBar.vue'
-import PartnersSection from './components/PartnersSection.vue'
+import PartnersSection from './components/sections/PartnersSection.vue'
 </script>
