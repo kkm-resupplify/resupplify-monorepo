fbb7d7b592bfd7997afa5602f6e13aa8e3d2f7a5
kamil_pula <sorennx@gmail.com>
2023-08-14 13:19:53 +0200

    Added BestsellersSection component

---
+++ apps/portal/src/components/core/product/card/ProductCardList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="product-card-list">mcardlisthere</div>
+</template>
+
+<script setup lang="ts">
+// Components
+import MCardList from '@sharedMolecules/cards/MCardList.vue'
+
+const props = defineProps({
+  products: {
+    type: Array<object>,
+    required: true
+  }
+})
+</script>
---
+++ apps/portal/src/views/Home/components/sections/BestsellersSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bestsellers-section">
+    <a-header
+      :title="$t('product.promotion.bestsellers')"
+      :subtitle="$t('product.promotion.topSellingProducts')"
+    />
+    <product-card-list :products="products" />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Components
+import AHeader from '@sharedAtoms/header/AHeader.vue'
+import ProductCardList from '@/components/core/product/card/ProductCardList.vue'
+
+const products = {}
+</script>
+
+<style scoped lang="scss">
+.bestsellers-section {
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  gap: $global-spacing-30;
+  align-self: stretch;
+
+  padding: $global-spacing-30 $global-spacing-40;
+
+  background-color: $global-colors-white;
+}
+</style>
