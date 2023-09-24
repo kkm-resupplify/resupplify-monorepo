28e772faf6fdb54ed92bc5075c6859a0d5e5a207
kamil_pula <sorennx@gmail.com>
2023-09-24 13:48:28 +0200

    Added test view

---
+++ apps/portal/src/views/Test/TestView.vue
@@ -0,0 +1,16 @@
+<template>
+  <basic-view-layout class="test-view">
+    <template #body> </template>
+  </basic-view-layout>
+</template>
+
+<script setup lang="ts">
+// Layouts
+import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
+</script>
+
+<style lang="scss" scoped>
+.test-view {
+  display: flex;
+}
+</style>
