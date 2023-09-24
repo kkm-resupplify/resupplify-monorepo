e909956c41c9834662edde33edaa64617bd9faea
kamil_pula <sorennx@gmail.com>
2023-09-24 20:48:19 +0200

    Update HomeView.spec.ts

--- apps/portal/src/views/Home/__tests__/HomeView.spec.ts
+++ apps/portal/src/views/Home/__tests__/HomeView.spec.ts
@@ -1,20 +1,20 @@
-// import { describe, it, expect } from 'vitest'
-// import { mount } from '@vue/test-utils'
-// import HomeView from '../HomeView.vue'
-// import { createTestingPinia } from '@pinia/testing'
-// import { useUserStore } from '@/stores/user/useUserStore'
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import HomeView from '../HomeView.vue'
+import { createTestingPinia } from '@pinia/testing'
+import { useUserStore } from '@/stores/user/useUserStore'
 
-// describe('HomeView - Integration', () => {
-//   it('renders text properly', async () => {
-//     const wrapper = mount(HomeView, {
-//       global: {
-//         plugins: [createTestingPinia()]
-//       }
-//     })
-//     const store = useUserStore()
-//     store.email = 'test@pinia.com'
-//     // await wrapper.vm.$nextTick()
+describe('HomeView - Integration', () => {
+  it('renders text properly', async () => {
+    const wrapper = mount(HomeView, {
+      global: {
+        plugins: [createTestingPinia()]
+      }
+    })
+    const store = useUserStore()
+    store.email = 'test@pinia.com'
+    // await wrapper.vm.$nextTick()
 
-//     // expect(wrapper.text()).toContain('@pinia.com')
-//   })
-// })
+    // expect(wrapper.text()).toContain('@pinia.com')
+  })
+})
