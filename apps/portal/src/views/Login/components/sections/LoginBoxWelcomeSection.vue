7ee94d8dcdb201b25078f65639102ceeeba1af49
krl8456 <karolrak50@gmail.com>
2023-10-04 20:11:02 +0200

    change background of WelcomeSection

--- apps/portal/src/views/Login/components/sections/LoginBoxWelcomeSection.vue
+++ apps/portal/src/views/Login/components/sections/LoginBoxWelcomeSection.vue
@@ -1,30 +1,82 @@
 <template>
-  <div class="login-box-welcome-section">
-    <login-header header-text="Welcome back" />
-  </div>
+  <Transition type="animation" name="slide">
+    <div class="login-box-welcome-section">
+      <div class="login-box-welcome-section__top">
+        <span v-text="$t('auth.slogan.createAccount')" />
+      </div>
+    </div>
+  </Transition>
 </template>
 
-<script setup lang="ts">
-import LoginHeader from '../LoginHeader.vue'
-</script>
-
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .login-box-welcome-section {
   display: flex;
-  box-sizing: border-box;
   flex-direction: column;
-  padding-inline: $global-spacing-40;
   align-self: stretch;
+  box-sizing: border-box;
   margin-bottom: $global-spacing-70;
+  padding-inline: $global-spacing-40;
   background: var(--primary-gradient);
+  background-size: 400% 400%;
   border-radius: $global-border-radius-20 $global-border-radius-20 0 0;
+  animation: gradient-movement 15s ease infinite;
+
+  &:hover {
+    :deep(span) {
+      transform: translateX(10px);
+      transition: transform 1.5s ease-out;
+    }
+  }
+
+  :deep(span) {
+    transform: translateX(0);
+    color: $themes-dark-text-primary;
+    transition: transform 0.9s ease-in;
+    align-self: stretch;
+
+    font-family: Syne, sans-serif;
+    font-size: 64px;
+    font-weight: 400;
+    font-style: normal;
+    line-height: normal;
+    color: var(--themes-light-background-primary-default, #f5f5f5);
+  }
+
+  &__top {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    align-self: stretch;
+    justify-content: center;
+    text-align: center;
+
+    height: 298px;
+  }
 
   @media (#{map-get($screen-breakpoints, 'md-and-up')}) {
     width: 50%;
     margin-bottom: 0;
+    padding-left: $global-spacing-70;
     text-align: left;
     border-radius: $global-border-radius-20 0 0 $global-border-radius-20;
-    padding-left: $global-spacing-70;
+
+    .login-box-welcome-section__top {
+      text-align: left;
+    }
+  }
+}
+
+@keyframes gradient-movement {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
   }
 }
 </style>
