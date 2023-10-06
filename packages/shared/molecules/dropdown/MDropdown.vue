522796bed445639c1fd7afc18b3d6c4843ada9e2
kamil_pula <sorennx@gmail.com>
2023-10-06 23:04:20 +0200

    Create MDropdown.vue

---
+++ packages/shared/molecules/dropdown/MDropdown.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <slot name="activator">Kliknij aby otworzyc</slot>
+
+    <slot name="body">Itemy w dropdownie</slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
