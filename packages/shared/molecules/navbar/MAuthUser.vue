5e8a6391c145f4a692e53b8799b5ae9dfc2324f5
krl8456 <karolrak50@gmail.com>
2023-10-06 20:26:33 +0200

    add items to the menu

--- packages/shared/molecules/navbar/MAuthUser.vue
+++ packages/shared/molecules/navbar/MAuthUser.vue
@@ -1,3 +1,8 @@
 <template>
   <a-icon icon="supervised_user_circle" size="x-large" />
+  <m-auth-user-options />
 </template>
+
+<script setup lang="ts">
+import MAuthUserOptions from './MAuthUserOptions.vue'
+</script>
---
+++ packages/shared/molecules/navbar/MAuthUserOptions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="m-auth-user-options">
+    <a-link link="/user/id" :text="$t('auth.user.account')" size="medium" />
+    <a-link link="/user/id/settings" :text="$t('auth.user.settings')" size="medium" />
+    <a-link link="/user/id/companies" :text="$t('auth.user.companies')" size="medium" />
+    <a-button :text="$t('auth.user.logout')" size="medium" />
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.m-auth-user-options {
+  display: flex;
+  flex-direction: column;
+}
+</style>
