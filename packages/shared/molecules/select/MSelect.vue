e8353392c247633cb581b659e2b1e0abab16fe7f
Kamil Pula <kamil.pula.dev@gmail.com>
2023-10-09 20:17:28 +0200

    Intialized MSelect

---
+++ packages/shared/molecules/select/MSelect.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="generateClasses"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useField } from 'vee-validate'
+import { useClassComposable } from '@sharedComposables/class/useClassComposable'
+
+// Variables
+const baseClass = 'm-select'
+
+// Composables
+const { generateClassNames } = useClassComposable()
+
+// Computed
+const generateClasses = computed(() => {
+  return generateClassNames(baseClass, [])
+})
+</script>
+
+<style lang="scss" scoped>
+.m-select {
+  display: flex;
+}
+</style>
