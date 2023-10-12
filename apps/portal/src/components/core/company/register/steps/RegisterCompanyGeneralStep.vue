054692e17d2906dc0a2964c32c1ee8271d3ab90e
kamil_pula <sorennx@gmail.com>
2023-10-12 20:01:29 +0200

    Added initial store

--- apps/portal/src/components/core/company/register/steps/RegisterCompanyGeneralStep.vue
+++ apps/portal/src/components/core/company/register/steps/RegisterCompanyGeneralStep.vue
@@ -32,18 +32,50 @@
       </v-form>
     </template>
 
-    <template #footer>Melo</template>
+    <template #footer>
+      <div class="register-company-general-step__footer">
+        <a-button size="large" :text="$t('global.back')" @click="handlePreviousStep" />
+
+        <a-button size="large" :text="$t('global.next')" @click="handleNextStep" />
+      </div>
+    </template>
   </m-stepper-step-content>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRegisterCompanyStore } from '@/stores/company/useRegisterCompanyStore'
+
+const emits = defineEmits(['next-step', 'previous-step', 'general-step-data'])
+// Variables
+const registerCompanyStore = useRegisterCompanyStore()
+
+// Methods
+const handleNextStep = () => {
+  emits('next-step')
+}
+
+const handlePreviousStep = () => {
+  emits('previous-step')
+}
+</script>
 
 <style lang="scss" scoped>
 .register-company-general-step {
+  display: flex;
+  flex-direction: column;
+  gap: $global-spacing-50;
+
   &__body {
     display: flex;
     flex-direction: column;
     gap: $global-spacing-40;
+  }
+
+  &__footer {
+    display: flex;
+    flex: 1;
+    gap: $global-spacing-80;
+    justify-content: center;
   }
 }
 </style>
