73b82ad6477afdd0dc8b2fd622ec02d5ca9cf668
krl8456 <karolrak50@gmail.com>
2023-10-16 22:31:14 +0200

    Added links to user settings

--- apps/portal/src/views/User/Settings/components/UserSettingsBody.vue
+++ apps/portal/src/views/User/Settings/components/UserSettingsBody.vue
@@ -1,12 +1,29 @@
-import { reactive } from 'vue';
 <template>
   <div class="user-settings-body">
-
+    <m-tabs>
+      <template #tabs>
+        <a-link
+          v-for="(item, index) in profileLinks"
+          :key="index"
+          :link="item.link"
+          :text="item.text"
+        />
+      </template>
+    </m-tabs>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue'
+import { useI18n } from 'vue-i18n';
 
+// Variables
+const { t } = useI18n()
+const profileLinks = reactive([
+  { text: t('userSettings.profile'), link: '/settings/profile' },
+  { text: t('userSettings.preferences'), link: '/settings/preferences' },
+  { text: t('userSettings.myCompany'), link: '/settings/mycompany' }
+])
 </script>
 
 <style scoped lang="scss"></style>
