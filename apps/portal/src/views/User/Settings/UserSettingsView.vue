4a2fcb8ba8f5fa711e487a36193c0e8a228e094c
krl8456 <karolrak50@gmail.com>
2023-10-16 18:01:39 +0200

    Created UserSettingsView

---
+++ apps/portal/src/views/User/Settings/UserSettingsView.vue
@@ -0,0 +1,15 @@
+<template>
+    <basic-view-layout>
+
+    <template #body>
+      
+    </template>
+
+  </basic-view-layout>
+</template>
+<script setup lang="ts">
+  
+</script>
+<style scoped lang="scss">
+
+</style>
