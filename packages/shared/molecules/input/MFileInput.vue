687732922b7e162a44d2cd0670b590eafbcccf39
Kamil Pula <kamil.pula.dev@gmail.com>
2023-10-16 17:31:16 +0200

    Added initial MFileInput

---
+++ packages/shared/molecules/input/MFileInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="generateClasses"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useClassComposable } from '@sharedComposables/class/useClassComposable'
+
+const baseClass = 'm-file-input'
+
+const { generateClassNames } = useClassComposable()
+
+const generateClasses = computed(() => {
+  return generateClassNames(baseClass, [])
+})
+</script>
+
+<style lang="scss" scoped>
+.m-file-input {
+  display: flex;
+}
+</style>
