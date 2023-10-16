b7538a43144c2d511a8fce8937c9833c1f49bf3f
krl8456 <karolrak50@gmail.com>
2023-10-16 23:43:57 +0200

    Set props in UserSettingsContent

--- apps/portal/src/views/User/Settings/components/UserSettingsContent.vue
+++ apps/portal/src/views/User/Settings/components/UserSettingsContent.vue
@@ -1,5 +1,36 @@
 <template></template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  userImg: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  birthDate: {
+    type: String,
+    required: true
+  },
+  sex: {
+    type: String,
+    required: true
+  }
+})
+</script>
 
 <style scoped lang="scss"></style>
