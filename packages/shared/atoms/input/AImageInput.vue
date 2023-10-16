87cb0c9382e05444778748d35018ffeb9c480e82
Kamil Pula <kamil.pula.dev@gmail.com>
2023-10-16 17:32:09 +0200

    Added initial AImageInput

---
+++ packages/shared/atoms/input/AImageInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="generateClasses"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useClassComposable } from '@sharedComposables/class/useClassComposable'
+
+const baseClass = 'a-image-input'
+
+const { generateClassNames } = useClassComposable()
+
+const generateClasses = computed(() => {
+  return generateClassNames(baseClass, [])
+})
+</script>
+
+<style lang="scss" scoped>
+.a-image-input {
+  display: flex;
+}
+</style>
