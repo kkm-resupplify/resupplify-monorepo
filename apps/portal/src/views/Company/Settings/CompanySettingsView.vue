f82cd8fc26045e261780215b828231d824d82013
krl8456 <karolrak50@gmail.com>
2023-10-17 19:39:17 +0200

    Created CompanySettingsView

---
+++ apps/portal/src/views/Company/Settings/CompanySettingsView.vue
@@ -0,0 +1,12 @@
+<template>
+    <basic-view-layout>
+      <template #body>
+        <company-settings-body />
+      </template>
+    </basic-view-layout>
+  </template>
+  <script setup lang="ts">
+  import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
+  import UserSettingsBody from './components/UserSettingsBody.vue'
+  </script>
+  <style scoped lang="scss"></style>
