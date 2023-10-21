1f4616e09e3915449785fe1f3fb2c6706a9cca45
kamil_pula <sorennx@gmail.com>
2023-10-21 20:16:05 +0200

    Improved style and layout

--- apps/portal/src/components/core/navigation/items/NavigationBarMenu.vue
+++ apps/portal/src/components/core/navigation/items/NavigationBarMenu.vue
@@ -1,33 +1,50 @@
 <template>
-  <a-dropdown>
+  <a-dropdown class="navigation-bar-menu">
     <template #activator>
-      <div
-        style="
-          display: flex;
-          flex-direction: column;
-          width: 200px;
-          background-color: var(--primary);
-        "
-      >
-        Elo
+      <div class="navigation-bar-menu__header">
+        <div class="navigation-bar-menu__user-data">
+          <span v-text="fullName" />
+
+          <span v-text="userStore.email" />
+        </div>
+
+        <div class="navigation-bar-menu__user-avatar">
+          <div class="img" />
+        </div>
       </div>
     </template>
 
     <template #content>
-      <div
-        style="
-          display: flex;
-          flex-direction: column;
-          width: 200px;
-          background-color: var(--primary);
-        "
-      >
-        <div v-for="i in 5" :key="i" v-text="i" />
+      <div style="display: flex; flex-direction: column; background-color: var(--primary); flex: 1">
+        <div v-for="i in 5" :key="i" style="display: flex" v-text="i" />
       </div>
     </template>
   </a-dropdown>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useUserStore } from '@/stores/user/useUserStore'
+import { useFullNameComposable } from '@sharedComposables/user/useUserDataComposable'
+// Variables
+const userStore = useUserStore()
+const { userFullName } = useFullNameComposable()
 
-<style lang="scss" scoped></style>
+// Computed
+const fullName = computed(() => {
+  return userFullName(userStore.getUserDetails?.first_name, userStore.getUserDetails?.last_name)
+})
+</script>
+
+<style lang="scss" scoped>
+.navigation-bar-menu {
+  &__header {
+    display: flex;
+  }
+
+  &__user-data {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
