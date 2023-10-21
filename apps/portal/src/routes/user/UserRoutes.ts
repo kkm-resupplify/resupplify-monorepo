07f333e06786bd2259bb874005ebe9e4ae0fc9ea
krl8456 <karolrak50@gmail.com>
2023-10-21 18:32:10 +0200

    Added route for UserDetails

--- apps/portal/src/routes/user/UserRoutes.ts
+++ apps/portal/src/routes/user/UserRoutes.ts
@@ -1,13 +1,16 @@
 import type { RouteRecordRaw } from 'vue-router'
 import UserProfileView from '@/views/User/Profile/UserProfileView.vue'
 import BaseEnum from '@sharedEnums/BaseEnum'
+import UserDetailsViewVue from '@/views/UserDetails/UserDetailsView.vue'
 
 class UserProfileRouteEnum extends BaseEnum {
   static readonly USER_PROFILE: string = 'USER_PROFILE'
+  static readonly USER_DETAILS: string = 'USER_DETAILS'
 }
 
 const UserRoutes: RouteRecordRaw[] = [
-  { path: '/user/profile/:id', name: UserProfileRouteEnum.USER_PROFILE, component: UserProfileView }
+  { path: '/user/profile/:id', name: UserProfileRouteEnum.USER_PROFILE, component: UserProfileView },
+  { path: '/user/details', name: UserProfileRouteEnum.USER_DETAILS, component: UserDetailsViewVue }
 ]
 
 export default UserRoutes
