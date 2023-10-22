7815e68fac45ec571d2c4c1186c780c2b68ae453
krl8456 <karolrak50@gmail.com>
2023-10-22 14:05:28 +0200

    Added route for from to edit user details

--- apps/portal/src/routes/user/UserRoutes.ts
+++ apps/portal/src/routes/user/UserRoutes.ts
@@ -3,10 +3,12 @@ import UserProfileView from '@/views/User/Profile/UserProfileView.vue'
 import BaseEnum from '@sharedEnums/BaseEnum'
 import UserSettingsView from '@/views/User/Settings/UserSettingsView.vue'
 import { MustBeAuthenticated } from '@/routes/navigationGuards'
+import UserDetailsForm from '@/components/core/user/details/UserDetailsForm.vue'
 
 class UserProfileRouteEnum extends BaseEnum {
   static readonly USER_PROFILE: string = 'USER_PROFILE'
   static readonly USER_SETTINGS: string = 'USER_SETTINGS'
+  static readonly USER_SETTINGS_EDIT: string = 'USER_SETTINGS_EDIT'
 }
 
 const UserRoutes: RouteRecordRaw[] = [
@@ -19,6 +21,14 @@ const UserRoutes: RouteRecordRaw[] = [
     path: '/settings/profile/',
     name: UserProfileRouteEnum.USER_SETTINGS,
     component: UserSettingsView,
+    beforeEnter: () => {
+      return MustBeAuthenticated.guard()
+    }
+  },
+  {
+    path: '/settings/profile/edit',
+    name: UserProfileRouteEnum.USER_SETTINGS_EDIT,
+    component: UserDetailsForm,
     beforeEnter: () => {
       return MustBeAuthenticated.guard()
     }
