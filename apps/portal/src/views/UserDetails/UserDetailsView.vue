1d7c26b7aaee15925f6d80c9f434cdd32945a538
kamil_pula <sorennx@gmail.com>
2023-10-22 21:59:56 +0200

    Removed unecessary components

--- apps/portal/src/views/UserDetails/UserDetailsView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <basic-view-layout>
-    <template #body> ay lmao </template>
-  </basic-view-layout>
-</template>
-
-<script setup lang="ts">
-import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
-</script>
