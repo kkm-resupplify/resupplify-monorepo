0f6b4758baae3194eb51e1915b42bbfddfb17f50
kamil_pula <sorennx@gmail.com>
2023-10-24 23:00:15 +0200

    Updated layout, added translations

--- apps/portal/src/components/core/company/data/CompanyDataBlock.vue
+++ apps/portal/src/components/core/company/data/CompanyDataBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="company-data-block">
     <div class="company-data-block__section">
-      <a-title title="Company general data" />
+      <a-title :title="$t('company.data.generalInformation')" />
 
       <div class="company-data-block__section-items">
         <a-title
@@ -9,7 +9,38 @@
           :key="idx"
           variant="horizontal"
           :title="item.title"
-          :value="item.value"
+          :subtitle="item.value"
+          append-colon
+        />
+      </div>
+    </div>
+
+    <div class="company-data-block__section">
+      <a-title :title="$t('company.data.generalInformation')" />
+
+      <div class="company-data-block__section-items">
+        <a-title
+          v-for="(item, idx) in companyDetailsData"
+          :key="idx"
+          variant="horizontal"
+          :title="item.title"
+          :subtitle="item.value"
+          append-colon
+        />
+      </div>
+    </div>
+
+    <div class="company-data-block__section">
+      <a-title :title="$t('company.data.generalInformation')" />
+
+      <div class="company-data-block__section-items">
+        <a-title
+          v-for="(item, idx) in companyContactData"
+          :key="idx"
+          variant="horizontal"
+          :title="item.title"
+          :subtitle="item.value"
+          append-colon
         />
       </div>
     </div>
@@ -22,9 +53,10 @@ import type { PropType } from 'vue'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-defineProps({
+const props = defineProps({
   company: {
-    type: [Object as PropType<CompanyData>]
+    type: [Object as PropType<CompanyData>],
+    required: true
   }
 })
 // Variables
@@ -32,7 +64,30 @@ const { t } = useI18n()
 
 // Computed
 const companyGeneralData = computed(() => {
-  return [{ title: t(''), value: '' }]
+  return [
+    { title: t('company.data.name'), value: props.company.name },
+    { title: t('company.data.description'), value: props.company.description },
+    { title: t('company.data.shortDescription'), value: props.company.shortDescription }
+    // { title: t('company.data.logo'), value: props.company.details.logo }
+  ]
+})
+
+const companyDetailsData = computed(() => {
+  return [
+    // { title: t('company.data.country'), value: props.company.details.countryId },
+    { title: t('company.data.address'), value: props.company.details.address },
+    { title: t('company.data.category'), value: props.company.details.companyCategoryId },
+    { title: t('company.data.tin'), value: props.company.details.tin }
+  ]
+})
+
+const companyContactData = computed(() => {
+  return [
+    { title: t('company.data.contactPerson'), value: props.company.details.contactPerson },
+    { title: t('company.data.email'), value: props.company.details.email },
+    { title: t('company.data.externalWebsite'), value: props.company.details.externalWebsite },
+    { title: t('company.data.phoneNumber'), value: props.company.details.phoneNumber }
+  ]
 })
 </script>
 
@@ -40,5 +95,6 @@ const companyGeneralData = computed(() => {
 .company-data-block {
   display: flex;
   flex-direction: column;
+  gap: $global-spacing-50;
 }
 </style>
