01cf4a6cbaa84974184c91a5f741aad8a9d00a8b
kamil_pula <sorennx@gmail.com>
2023-10-25 00:53:25 +0200

    Updated styles and layout

--- apps/portal/src/components/core/user/settings/myCompany/UserSettingsMyCompanyTab.vue
+++ apps/portal/src/components/core/user/settings/myCompany/UserSettingsMyCompanyTab.vue
@@ -2,26 +2,12 @@
   <div class="user-settings-my-company-tab">
     <div class="user-settings-my-company-tab__content">
       <template v-if="!userStore.hasCompany">
-        <template v-if="!showRegistrationForm">
-          <join-company-section />
+        <join-company-section
+          v-if="!showRegistrationForm"
+          @show-registration-form="handleShowRegistrationForm"
+        />
 
-          <div class="">
-            <a-title
-              size="large"
-              title="Register company"
-              subtitle="You might also want to register you company. In that case, you can begin the registration process by clicking the button below - you may cancel it at any time until."
-            />
-
-            <a-button
-              variant="primary"
-              size="x-large"
-              text="Begin"
-              @click="handleShowRegistrationForm"
-            />
-          </div>
-        </template>
-
-        <register-company v-else />
+        <register-company v-else @cancel="handleShowRegistrationForm(false)" />
       </template>
 
       <my-company-section v-else />
@@ -30,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import { useUserStore } from '@/stores/user/useUserStore'
 import JoinCompanySection from '@/components/core/user/settings/myCompany/sections/JoinCompanySection.vue'
 import MyCompanySection from '@/components/core/user/settings/myCompany/sections/MyCompanySection.vue'
@@ -41,8 +27,8 @@ const userStore = useUserStore()
 const showRegistrationForm = ref(false)
 
 // Methods
-const handleShowRegistrationForm = () => {
-  showRegistrationForm.value = true
+const handleShowRegistrationForm = (value: boolean) => {
+  showRegistrationForm.value = value
 }
 </script>
 
@@ -52,6 +38,12 @@ const handleShowRegistrationForm = () => {
     display: flex;
     flex-direction: column;
     gap: $global-spacing-50;
+    max-width: 1200px;
+
+    &__register-wrapper {
+      display: flex;
+      gap: $global-spacing-50;
+    }
   }
 }
 </style>
