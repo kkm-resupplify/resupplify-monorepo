c165a810e2641aadab699e1e4c7adeec3fc934c7
krl8456 <karolrak50@gmail.com>
2023-10-24 23:34:32 +0200

    Changed CompanyMemberListItem to dropdown

--- apps/portal/src/views/Company/Members/components/CompanyMemberList.vue
+++ apps/portal/src/views/Company/Members/components/CompanyMemberList.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="company-member-list">
+        <p>Members</p>
+        <company-member-list-item />
         <company-member-list-item />
     </div>
 </template>
@@ -7,3 +9,14 @@
 <script setup lang="ts">
 import CompanyMemberListItem from './CompanyMemberListItem.vue';
 </script>
+<style scoped lang="scss">
+    .company-member-list {
+        @include boxshadow-primary-2;
+        width: 60%;
+        margin-inline: auto;
+        display: flex;
+        flex-direction: column;
+        gap: $global-spacing-80;
+        padding: $global-spacing-90;
+    }
+</style>
--- apps/portal/src/views/Company/Members/components/CompanyMemberListItem.vue
+++ apps/portal/src/views/Company/Members/components/CompanyMemberListItem.vue
@@ -1,20 +1,44 @@
 <template>
     <div class="company-member-list-item">
-        <div class="company-member-list-item__header">
-            <a-title title="email" />
-        </div>
-        <div class="company-member-list-item__details">
-            <a-title title="firstName" size="medium"/>
-            <a-title title="lastName" size="medium"/>
-            <a-title title="phoneNumber" size="medium"/>
-            <a-title title="birthDate" size="medium"/>
-            <a-title title="sex" size="medium"/>
-        </div>
+        <a-dropdown class="company-member-list-item__dropdown">
+            <template #activator>
+                <div class="company-member-list-item__header">
+                    <a-title title="email" />
+                </div>
+            </template>
+            <template #content>
+                <div class="company-member-list-item__details">
+                    <a-title title="firstName" size="medium"/>
+                    
+                    <a-title title="lastName" size="medium"/>
+                    
+                    <a-title title="phoneNumber" size="medium"/>
+                    
+                    <a-title title="birthDate" size="medium"/>
+                    
+                    <a-title title="sex" size="medium"/>
+                </div>
+            </template>
+        </a-dropdown>
     </div>
 </template>
 
 <style scoped lang="scss">
 .company-member-list-item {
+    &__header, &__details {
+        padding: $global-spacing-50;
+        width: 100%;
+    }
 
+    &__header {
+        background-color: var(--secondary-2);
+    }
+
+    &__details {
+        display: flex;
+        flex-direction: column;
+        gap: $global-spacing-50;
+        background-color: var(--secondary-1);
+    }
 }
 </style>
