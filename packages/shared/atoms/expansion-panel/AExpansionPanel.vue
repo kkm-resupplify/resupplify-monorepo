dfba5aa630fa9158154bf4a72c5f9b9f6cf511fb
krl8456 <karolrak50@gmail.com>
2023-10-24 23:40:40 +0200

    Added AExpansionPanel atom component

---
+++ packages/shared/atoms/expansion-panel/AExpansionPanel.vue
@@ -0,0 +1,51 @@
+<template>
+    <div :class="generateClasses">
+      <div class="a-dropdown__activator" @click="toggleShowContent">
+        <slot name="activator" />
+      </div>
+  
+      <div v-if="showContent" class="a-dropdown__content">
+        <slot name="content" />
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { computed, ref } from 'vue'
+  import { useClassComposable } from '@sharedComposables/class/useClassComposable'
+  
+  const props = defineProps({})
+  
+  // Variables
+  const baseClass = 'a-dropdown'
+  const { generateClassNames } = useClassComposable()
+  const showContent = ref<boolean>(false)
+  
+  // Computed
+  const generateClasses = computed(() => {
+    return generateClassNames(baseClass, [])
+  })
+  
+  const toggleShowContent = () => {
+    showContent.value = !showContent.value
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  .a-dropdown {
+    cursor: pointer;
+    display: inline-block;
+  
+    &__activator {
+      user-select: none;
+      display: flex;
+    }
+  
+    &__content {
+      display: flex;
+      width: 100%;
+      padding: $global-spacing-10;
+    }
+  }
+  </style>
+  
