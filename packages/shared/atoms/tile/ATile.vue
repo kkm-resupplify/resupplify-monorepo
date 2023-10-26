1e89b0abb6a54c89d3a6a6aa95421471b87eb3da
kamil_pula <sorennx@gmail.com>
2023-10-26 21:49:17 +0200

    Initialized ATile

---
+++ packages/shared/atoms/tile/ATile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="generateClasses">ATile</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useClassComposable } from '@sharedComposables/class/useClassComposable'
+
+const props = defineProps({})
+
+// Variables
+const baseClass = 'a-tile'
+const { generateClassNames } = useClassComposable()
+
+// Computed
+const generateClasses = computed(() => {
+  return generateClassNames(baseClass, [])
+})
+</script>
+
+<style lang="scss" scoped>
+.a-tile {
+  display: flex;
+}
+</style>
