f4bd6cfa145a77e17be18dec328a2dcedc98f901
krl8456 <karolrak50@gmail.com>
2023-10-26 22:51:43 +0200

    Added methods for using CompanyMembersService

---
+++ apps/portal/src/stores/company/useCompanyMembersService.ts
@@ -0,0 +1,22 @@
+import type { CompanyMembersData } from '@/interfaces/company/CompanyMembersInterface'
+import { defineStore } from 'pinia'
+
+export const useRegisterCompanyStore = defineStore({
+  id: 'companyMembers',
+
+  state: (): CompanyMembersData => ({
+    users: []
+  }),
+
+  getters: {
+    getCompanyMembers: (state: CompanyMembersData) => state
+  },
+
+  actions: {
+    setCompanyMembers(companyMembersData: CompanyMembersData) {
+      this.users = companyMembersData.users
+    }
+  },
+
+  persist: true
+})
