a854a7e8cb040394f75eaac91b4bacf1027a8095
krl8456 <karolrak50@gmail.com>
2023-11-01 18:01:50 +0100

    Added translations for fields in WarehouseEditor form

---
+++ apps/portal/src/views/Company/Warehouses/components/WarehouseEditorForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <o-form>
+        <template #body>
+            <div class="warehouse-editor-form__body">
+                <a-title :title="">
+            </div>
+        </template>
+    </o-form>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
