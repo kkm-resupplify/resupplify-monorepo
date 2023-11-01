01de1444ba079dd7f7f6ddb5f7852a0039c26721
krl8456 <karolrak50@gmail.com>
2023-11-02 00:29:48 +0100

    Corrected translations and styles again

--- apps/portal/src/components/core/company/management/warehouse/editor/components/WarehouseEditorForm.vue
+++ apps/portal/src/components/core/company/management/warehouse/editor/components/WarehouseEditorForm.vue
@@ -1,31 +1,26 @@
 <template>
-  <o-form>
+  <o-form class="warehouse-editor-form">
     <template #body>
       <div class="warehouse-editor-form__body">
-        <a-title
-          :title="$t('company.management.navigation.warehouses.editor.title')"
-          size="x-large"
-        />
+        <a-title :title="$t('company.management.warehouse.editor.title')" size="x-large" />
 
         <m-text-field
           name="warehouseName"
           input-type="text"
-          :label="$t('company.management.navigation.warehouses.editor.nameLabel')"
-          :placeholder="$t('company.management.navigation.warehouses.editor.namePlaceholder')"
+          :label="$t('company.management.warehouse.editor.nameLabel')"
+          :placeholder="$t('company.management.warehouse.editor.namePlaceholder')"
           rules="required"
         />
 
         <m-text-area
           name="warehouseDescription"
           rules="required|min:16|max:255"
-          :label="$t('company.management.navigation.warehouses.editor.descriptionLabel')"
-          :placeholder="
-            $t('company.management.navigation.warehouses.editor.descriptionPlaceholder')
-          "
+          :label="$t('company.management.warehouse.editor.descriptionLabel')"
+          :placeholder="$t('company.management.warehouse.editor.descriptionPlaceholder')"
         />
 
         <a-button
-          :text="$t('company.management.navigation.warehouses.editor.descriptionLabel')"
+          :text="$t('company.management.warehouse.editor.descriptionLabel')"
           button-type="submit"
           class="warehouse-editor-form__button"
           size="x-large"
@@ -38,15 +33,17 @@
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-.warehouse-editor-form__body {
-  display: flex;
-  flex-direction: column;
-  gap: $global-spacing-50;
-  min-width: 500px;
-}
+.warehouse-editor-form {
+  &__body {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-50;
+    min-width: 500px;
+  }
 
-.warehouse-editor-form__button {
-  display: flex;
-  justify-content: center;
+  &__button {
+    display: flex;
+    justify-content: center;
+  }
 }
 </style>
