ba619f81aee24fd5e78df8f3cf6faef9d71eeb5c
kamil_pula <sorennx@gmail.com>
2023-11-02 21:31:11 +0100

    Updated test view

--- apps/portal/src/views/Test/TestView.vue
+++ apps/portal/src/views/Test/TestView.vue
@@ -1,50 +1,11 @@
 <template>
   <basic-view-layout class="test-view">
-    <template #body>
-    </template>
+    <template #body></template>
   </basic-view-layout>
 </template>
 
 <script setup lang="ts">
 import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
-import TestForm from './TestForm.vue'
-import TestFormTwo from './TestFormTwo.vue'
-import RegisterCompany from '@/components/core/company/register/RegisterCompany.vue'
-const steps = [
-  {
-    stepInfo: {
-      title: 'Title1',
-      subtitle: 'Immaculate content',
-      description: 'Lorem ipsum 1 dolor sit amet omega lul'
-    },
-    component: TestForm
-  },
-  {
-    stepInfo: {
-      title: 'Title2',
-      subtitle: 'Subtitle2',
-      description: 'Lorem ipsum 1 dolor sit amet omega lul'
-    },
-    component: TestForm
-  },
-  {
-    stepInfo: {
-      title: 'Title3',
-      subtitle: 'Subtitle2',
-      description: 'Lorem ipsum 1 dolor sit amet omega lul'
-    },
-    component: TestForm
-  },
-  {
-    stepInfo: {
-      title: 'Title4',
-      subtitle: 'Subtitle4',
-      description: 'Lorem ipsum 1 dolor sit amet omega lul',
-      icon: 'check'
-    },
-    component: TestFormTwo
-  }
-]
 </script>
 
 <style lang="scss" scoped>
