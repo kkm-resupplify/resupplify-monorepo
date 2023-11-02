24d8867bba38eeffe72ab867a5882158236baa4a
kamil_pula <sorennx@gmail.com>
2023-11-02 21:29:38 +0100

    Added structure for the company warehouses

---
+++ apps/portal/src/components/core/company/management/warehouse/dashboard/section/content/CompanyWarehouseContentSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    CompanyWarehouseContentSection
+
+    <company-warehouse-list />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CompanyWarehouseList from '@/components/core/company/management/warehouse/dashboard/section/content/CompanyWarehouseList.vue'
+</script>
+
+<style scoped lang="scss"></style>
---
+++ apps/portal/src/components/core/company/management/warehouse/dashboard/section/header/CompanyWarehouseNeedingAttention.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>CompanyWarehouseNeedingAttention</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
