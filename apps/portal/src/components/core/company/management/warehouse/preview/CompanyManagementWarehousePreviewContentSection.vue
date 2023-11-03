5d2aa30c1b15bbefcb57514fd336bcc94a261a83
kamil_pula <sorennx@gmail.com>
2023-11-03 23:56:52 +0100

    Initialized components

---
+++ apps/portal/src/components/core/company/management/warehouse/preview/CompanyManagementWarehousePreviewContentSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="company-management-warehouse-preview-content-section">
+    CompanyManagementWarehousePreviewContentSection
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.company-management-warehouse-preview-content-section {
+  display: flex;
+}
+</style>
--- apps/portal/src/components/core/company/management/warehouse/preview/CompanyManagementWarehousePreviewPanel.vue
+++ apps/portal/src/components/core/company/management/warehouse/preview/CompanyManagementWarehousePreviewPanel.vue
@@ -1,11 +1,15 @@
 <template>
   <company-management-panel class="company-management-warehouse-preview-panel">
-    company-management-warehouse-preview-panel
+    <company-management-warehouse-preview-header-section />
+
+    <company-management-warehouse-preview-content-section />
   </company-management-panel>
 </template>
 
 <script setup lang="ts">
 import CompanyManagementPanel from '@/components/core/company/panel/CompanyManagementPanel.vue'
+import CompanyManagementWarehousePreviewHeaderSection from '@/components/core/company/management/warehouse/preview/CompanyManagementWarehousePreviewHeaderSection.vue'
+import CompanyManagementWarehousePreviewContentSection from '@/components/core/company/management/warehouse/preview/CompanyManagementWarehousePreviewContentSection.vue'
 </script>
 
 <style lang="scss" scoped>
