af814093c2ab645ea5ce5d76a0877a6979778ee1
kamil_pula <sorennx@gmail.com>
2023-11-04 19:47:44 +0100

    Updated listitem structure

--- apps/portal/src/components/core/company/management/warehouse/preview/list/WarehouseProductListItem.vue
+++ apps/portal/src/components/core/company/management/warehouse/preview/list/WarehouseProductListItem.vue
@@ -3,6 +3,17 @@
     <a-status-indicator :status="warehouseIndicatorStatus" />
 
     <a-title :title="product.name" />
+
+    <div class="warehouse-product-list-item__stats">
+      <div
+        v-for="(productStat, idx) in productStatList"
+        :key="idx"
+        class="warehouse-product-list-item__stat-item"
+      >
+        <a-title :title="productStat.title" />
+        <span v-text="productStat.value" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,14 +35,23 @@ const isProductActive = computed(() => {
 const productSafeQuantityRatio = computed(() => {
   return props.product.quantity / props.product.safeQuantity
 })
+
 const warehouseIndicatorStatus = computed(() => {
   if (isProductActive.value) {
-    if (productSafeQuantityRatio.value >= 1.2) return 1
-    else if (productSafeQuantityRatio.value >= 0.8) return 2
+    if (productSafeQuantityRatio.value >= 1) return 1
     else if (productSafeQuantityRatio.value === 0) return 3
+
+    return 2
   }
 
   return 0
+})
+
+const productStatList = computed(() => {
+  return [
+    { title: 'Current supply', value: props.product.quantity },
+    { title: 'Safe supply', value: props.product.quantity }
+  ]
 })
 </script>
 
@@ -39,7 +59,7 @@ const warehouseIndicatorStatus = computed(() => {
 .warehouse-product-list-item {
   display: flex;
   flex: 1;
-  gap: $global-spacing-20;
+  gap: $global-spacing-40;
   align-items: center;
 
   box-sizing: border-box;
@@ -47,5 +67,16 @@ const warehouseIndicatorStatus = computed(() => {
 
   border: 1px solid var(--font-primary);
   border-radius: $global-border-radius-10;
+
+  &__stats {
+    display: flex;
+    gap: $global-spacing-40;
+  }
+
+  &__stat-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
