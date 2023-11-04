2e14ab41264ca7c03db1f8003f121df4ff940316
kamil_pula <sorennx@gmail.com>
2023-11-04 23:33:11 +0100

    Updated AddWarehouseProductDialog style & added translations

--- apps/portal/src/components/core/company/management/warehouse/preview/dialog/AddWarehouseProductDialog.vue
+++ apps/portal/src/components/core/company/management/warehouse/preview/dialog/AddWarehouseProductDialog.vue
@@ -1,5 +1,8 @@
 <template>
-  <m-dialog ref="dialogRef" title="Test title">
+  <m-dialog
+    ref="dialogRef"
+    :title="$t('company.management.warehouse.preview.dialog.addProduct.title')"
+  >
     <template #activator>
       <a-button size="large" :text="$t('company.management.warehouse.preview.header.addProduct')" />
     </template>
@@ -7,10 +10,14 @@
     <div class="add-warehouse-product-dialog__content">
       <o-form :submit-callback="handleSubmitAddProduct">
         <template #body>
-          <div class="add-warehouse-product-dialog__input-group">
+          <div class="add-warehouse-product-dialog__form-body">
             <m-select
-              label="Product"
-              placeholder="Select product"
+              :label="$t('company.management.warehouse.preview.dialog.addProduct.selectProduct')"
+              :placeholder="
+                $t(
+                  'company.management.warehouse.preview.dialog.addProduct.selectProductPlaceholder'
+                )
+              "
               name="productId"
               :options="products"
               :validate="false"
@@ -18,22 +25,43 @@
 
             <div class="add-warehouse-product-dialog__quantity-settings">
               <m-text-field
-                placeholder="Enter safe quantity"
-                label="Safe quantity"
+                :label="
+                  $t('company.management.warehouse.preview.dialog.addProduct.safeQuantityLabel')
+                "
+                :placeholder="
+                  $t(
+                    'company.management.warehouse.preview.dialog.addProduct.safeQuantityPlaceholder'
+                  )
+                "
                 name="safeQuantity"
-                type="number"
+                input-type="number"
                 :validate="false"
               />
 
               <m-text-field
-                placeholder="Enter current quantity"
-                label="Quantity"
+                :label="
+                  $t('company.management.warehouse.preview.dialog.addProduct.currentSupplyLabel')
+                "
+                :placeholder="
+                  $t(
+                    'company.management.warehouse.preview.dialog.addProduct.currentSupplyPlaceholder'
+                  )
+                "
                 name="quantity"
-                type="number"
+                input-type="number"
                 :validate="false"
               />
             </div>
           </div>
+        </template>
+
+        <template #footer>
+          <a-button
+            type="submit"
+            :text="$t('company.management.warehouse.preview.dialog.addProduct.submit')"
+            size="x-large"
+            class="add-warehouse-product-dialog__submit"
+          />
         </template>
       </o-form>
     </div>
@@ -52,6 +80,29 @@ const dialogRef = ref<null | InstanceType<typeof MDialog>>(null)
 const products = computed(() => {
   return [
     { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
+    { id: 1, text: 'Product 1' },
     { id: 2, text: 'Product 2' }
   ]
 })
@@ -63,7 +114,7 @@ const closeDialog = () => {
 
 const handleSubmitAddProduct = (formData: Record<string, any>) => {
   console.log(formData)
-  closeDialog()
+  // closeDialog()
 }
 </script>
 
@@ -71,23 +122,30 @@ const handleSubmitAddProduct = (formData: Record<string, any>) => {
 .add-warehouse-product-dialog {
   &__content {
     @include respond-to('sm-and-up') {
-      width: 800px;
-      min-height: 600px;
+      width: 600px;
+      min-height: 280px;
     }
 
+    display: flex;
     width: 380px;
   }
 
-  &__input-group {
+  &__form-body {
+    overflow: clip;
     display: flex;
+    flex: 1;
     flex-direction: column;
-    gap: $global-spacing-20;
+    gap: $global-spacing-50;
   }
 
   &__quantity-settings {
     display: flex;
     flex-direction: row;
     gap: $global-spacing-50;
+  }
+
+  &__submit {
+    width: 100%;
   }
 }
 </style>
