33fa629ec7356f836dd74c3655be0a168132324e
kamil_pula <sorennx@gmail.com>
2023-11-04 20:28:10 +0100

    Updated warehouse product list & item

--- apps/portal/src/components/core/company/management/warehouse/preview/list/WarehouseProductList.vue
+++ apps/portal/src/components/core/company/management/warehouse/preview/list/WarehouseProductList.vue
@@ -14,26 +14,26 @@ import WarehouseProductListItem from '@/components/core/company/management/wareh
 
 // Variables
 const warehouseProducts = reactive([
-  { id: 1, name: 'Product 1', quantity: 1200, safeQuantity: 500, status: 0 },
-  { id: 2, name: 'Product 2', quantity: 340, safeQuantity: 300, status: 1 },
-  { id: 3, name: 'Product 3', quantity: 0, safeQuantity: 100, status: 2 },
-  { id: 4, name: 'Product 4', quantity: 100, safeQuantity: 50, status: 1 },
-  { id: 5, name: 'Product 5', quantity: 500, safeQuantity: 250, status: 0 },
-  { id: 6, name: 'Product 6', quantity: 70, safeQuantity: 100, status: 1 },
-  { id: 7, name: 'Product 7', quantity: 150, safeQuantity: 75, status: 1 },
-  { id: 8, name: 'Product 8', quantity: 90, safeQuantity: 150, status: 2 },
-  { id: 9, name: 'Product 9', quantity: 1, safeQuantity: 125, status: 1 },
-  { id: 10, name: 'Product 10', quantity: 400, safeQuantity: 200, status: 1 },
-  { id: 11, name: 'Product 11', quantity: 100, safeQuantity: 50, status: 1 },
-  { id: 12, name: 'Product 12', quantity: 0, safeQuantity: 250, status: 1 },
-  { id: 13, name: 'Product 13', quantity: 200, safeQuantity: 100, status: 1 },
-  { id: 14, name: 'Product 14', quantity: 150, safeQuantity: 75, status: 0 },
-  { id: 15, name: 'Product 15', quantity: 300, safeQuantity: 150, status: 1 },
-  { id: 16, name: 'Product 16', quantity: 250, safeQuantity: 125, status: 0 },
-  { id: 17, name: 'Product 17', quantity: 400, safeQuantity: 200, status: 1 },
-  { id: 18, name: 'Product 18', quantity: 100, safeQuantity: 50, status: 2 },
-  { id: 19, name: 'Product 19', quantity: 500, safeQuantity: 250, status: 1 },
-  { id: 20, name: 'Product 20', quantity: 0, safeQuantity: 100, status: 0 }
+  { id: 1, name: 'Shampoo', quantity: 1200, safeQuantity: 500, status: 0 },
+  { id: 2, name: 'Toothpaste', quantity: 340, safeQuantity: 300, status: 1 },
+  { id: 3, name: 'Soap', quantity: 0, safeQuantity: 100, status: 2 },
+  { id: 4, name: 'Cereal', quantity: 100, safeQuantity: 50, status: 1 },
+  { id: 5, name: 'Detergent', quantity: 500, safeQuantity: 250, status: 0 },
+  { id: 6, name: 'Milk', quantity: 70, safeQuantity: 100, status: 1 },
+  { id: 7, name: 'Bread', quantity: 150, safeQuantity: 75, status: 1 },
+  { id: 8, name: 'Canned Soup', quantity: 90, safeQuantity: 150, status: 2 },
+  { id: 9, name: 'Toilet Paper', quantity: 1, safeQuantity: 125, status: 1 },
+  { id: 10, name: 'Bottled Water', quantity: 400, safeQuantity: 200, status: 1 },
+  { id: 11, name: 'Cereal Bars', quantity: 100, safeQuantity: 50, status: 1 },
+  { id: 12, name: 'Juice', quantity: 0, safeQuantity: 250, status: 1 },
+  { id: 13, name: 'Canned Vegetables', quantity: 200, safeQuantity: 100, status: 1 },
+  { id: 14, name: 'Chips', quantity: 150, safeQuantity: 75, status: 0 },
+  { id: 15, name: 'Coffee', quantity: 300, safeQuantity: 150, status: 1 },
+  { id: 16, name: 'Tea', quantity: 250, safeQuantity: 125, status: 0 },
+  { id: 17, name: 'Paper Towels', quantity: 400, safeQuantity: 200, status: 1 },
+  { id: 18, name: 'Shaving Cream', quantity: 100, safeQuantity: 50, status: 2 },
+  { id: 19, name: 'Laundry Detergent', quantity: 500, safeQuantity: 250, status: 1 },
+  { id: 20, name: 'Candles', quantity: 0, safeQuantity: 100, status: 0 }
 ])
 </script>
 
--- apps/portal/src/components/core/company/management/warehouse/preview/list/WarehouseProductListItem.vue
+++ apps/portal/src/components/core/company/management/warehouse/preview/list/WarehouseProductListItem.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="warehouse-product-list-item">
+  <a-list-item-wrapper>
     <a-status-indicator :status="warehouseIndicatorStatus" />
 
-    <a-title :title="product.name" />
+    <a-list-item-title-section title="Product name" :value="product.name" :basis="20" />
 
     <div class="warehouse-product-list-item__stats">
-      <div
+      <a-list-item-title-section
         v-for="(productStat, idx) in productStatList"
         :key="idx"
-        class="warehouse-product-list-item__stat-item"
-      >
-        <a-title :title="productStat.title" />
-        <span v-text="productStat.value" />
-      </div>
+        :title="productStat.title"
+        :value="productStat.value"
+      />
     </div>
-  </div>
+
+    <edit-warehouse-product-dialog />
+  </a-list-item-wrapper>
 </template>
 
 <script setup lang="ts">
+import EditWarehouseProductDialog from '@/components/core/company/management/warehouse/preview/dialog/EditWarehouseProductDialog.vue'
 import { computed } from 'vue'
 
 const props = defineProps({
@@ -50,26 +51,16 @@ const warehouseIndicatorStatus = computed(() => {
 const productStatList = computed(() => {
   return [
     { title: 'Current supply', value: props.product.quantity },
-    { title: 'Safe supply', value: props.product.quantity }
+    { title: 'Safe supply', value: props.product.safeQuantity }
   ]
 })
 </script>
 
 <style scoped lang="scss">
 .warehouse-product-list-item {
-  display: flex;
-  flex: 1;
-  gap: $global-spacing-40;
-  align-items: center;
-
-  box-sizing: border-box;
-  padding: $global-spacing-40;
-
-  border: 1px solid var(--font-primary);
-  border-radius: $global-border-radius-10;
-
   &__stats {
     display: flex;
+    flex: 1;
     gap: $global-spacing-40;
   }
 
