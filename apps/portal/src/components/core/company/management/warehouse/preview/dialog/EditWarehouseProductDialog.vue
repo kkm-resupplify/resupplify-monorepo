34dcdb334e8dcbd59264a9b530bd415763893bd0
kamil_pula <sorennx@gmail.com>
2023-11-04 20:27:54 +0100

    Added initial EditWarehouseProductDialog

---
+++ apps/portal/src/components/core/company/management/warehouse/preview/dialog/EditWarehouseProductDialog.vue
@@ -0,0 +1,85 @@
+<template>
+  <m-dialog ref="dialogRef" title="Test title">
+    <template #activator>
+      <a-icon icon="more_vert" size="xx-large" hoverable />
+    </template>
+
+    <div class="edit-warehouse-product-dialog__content">
+      <o-form :submit-callback="handleSubmitAddProduct">
+        <template #body>
+          <div class="edit-warehouse-product-dialog__input-group">
+            <div class="edit-warehouse-product-dialog__quantity-settings">
+              <m-text-field
+                placeholder="Enter safe quantity"
+                label="Safe quantity"
+                name="safeQuantity"
+                type="number"
+                :validate="false"
+              />
+
+              <m-text-field
+                placeholder="Enter current quantity"
+                label="Quantity"
+                name="quantity"
+                type="number"
+                :validate="false"
+              />
+            </div>
+          </div>
+        </template>
+      </o-form>
+    </div>
+  </m-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import MDialog from '@sharedMolecules/dialog/MDialog.vue'
+
+// Variables
+// https://vuejs.org/guide/typescript/composition-api.html#typing-component-template-refs
+const dialogRef = ref<null | InstanceType<typeof MDialog>>(null)
+
+// Computed
+const products = computed(() => {
+  return [
+    { id: 1, text: 'Product 1' },
+    { id: 2, text: 'Product 2' }
+  ]
+})
+
+// Methods
+const closeDialog = () => {
+  dialogRef.value?.closeDialog()
+}
+
+const handleSubmitAddProduct = (formData: Record<string, any>) => {
+  console.log(formData)
+  closeDialog()
+}
+</script>
+
+<style lang="scss" scoped>
+.edit-warehouse-product-dialog {
+  &__content {
+    @include respond-to('sm-and-up') {
+      width: 800px;
+      min-height: 600px;
+    }
+
+    width: 380px;
+  }
+
+  &__input-group {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-20;
+  }
+
+  &__quantity-settings {
+    display: flex;
+    flex-direction: row;
+    gap: $global-spacing-50;
+  }
+}
+</style>
