849c7cf5822b104a598ec39993be603ec96143ac
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-11-05 20:06:09 +0100

    Added displaying list items that have status 2 or 3

---
+++ apps/portal/src/components/core/company/management/warehouse/dashboard/section/content/list/WarehouseNeedingAttentionList.vue
@@ -0,0 +1,27 @@
+<template>
+    <p>WarehouseNeedingAttention</p>
+  <warehouse-needing-attention-list-item
+    v-for="el in warehouseProductsNeedingAttention"
+    :key="el.id"
+    :warehouse="el"
+  />
+</template>
+
+<script setup lang="ts">
+import WarehouseNeedingAttentionListItem from './WarehouseNeedingAttentionListItem.vue'
+import { reactive, computed } from 'vue'
+
+const warehouseProducts = reactive([
+  { id: 1, name: `Warehouse ${1}`, status: 1 },
+  { id: 2, name: `Warehouse ${2}`, status: 2 },
+  { id: 3, name: `Warehouse ${3}`, status: 3 },
+  { id: 4, name: `Warehouse ${4}`, status: 4 },
+  { id: 5, name: `Warehouse ${5}`, status: 5 }
+])
+
+const warehouseProductsNeedingAttention = computed(() => {
+  return warehouseProducts.filter((el) => el.status === 2 || el.status === 3)
+})
+</script>
+
+<style scoped lang="scss"></style>
---
+++ apps/portal/src/components/core/company/management/warehouse/dashboard/section/content/list/WarehouseNeedingAttentionListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <m-tile :to="warehouseLink">
+    <div class="warehouse-list-item__header">
+      <a-status-indicator :status="warehouse.status" />
+    </div>
+
+    <div class="warehouse-list-item__content">{{ warehouse.name }}</div>
+  </m-tile>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { RouteNames } from '@/routes/index'
+
+const props = defineProps({
+  warehouse: { type: Object, required: true }
+})
+
+const warehouseLink = computed(() => {
+  return {
+    name: RouteNames.COMPANY_WAREHOUSE_PREVIEW,
+    params: {
+      id: props.warehouse.id
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
