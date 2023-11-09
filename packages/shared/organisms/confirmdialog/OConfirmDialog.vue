3e50b386ad18b9bc5d13916d5c01fae3fe2fae12
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-11-09 18:42:39 +0100

    Added props

--- packages/shared/organisms/confirmdialog/OConfirmDialog.vue
+++ packages/shared/organisms/confirmdialog/OConfirmDialog.vue
@@ -1,5 +1,20 @@
 <template></template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  itemName: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    validator: (value: string) => ['add', 'remove', 'delete'].includes(value)
+  },
+  text: {
+    type: String,
+    default: 'text'
+  }
+})
+</script>
 
 <style scoped lang="scss"></style>
