ee83edf92e06a9d79fa7a5b7fe3e9affa7dfa6b4
Kamil Pula <kamil.pula.dev@gmail.com>
2023-11-10 16:34:25 +0100

    Updated the button size

--- apps/portal/src/components/core/company/management/warehouse/preview/dialog/AddWarehouseProductDialog.vue
+++ apps/portal/src/components/core/company/management/warehouse/preview/dialog/AddWarehouseProductDialog.vue
@@ -4,10 +4,7 @@
     :title="$t('company.management.warehouse.preview.dialog.addProduct.title')"
   >
     <template #activator>
-      <a-button
-        size="x-large"
-        :text="$t('company.management.warehouse.preview.header.addProduct')"
-      />
+      <a-button size="large" :text="$t('company.management.warehouse.preview.header.addProduct')" />
     </template>
 
     <div class="add-warehouse-product-dialog__content">
