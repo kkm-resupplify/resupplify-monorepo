95eeaa75aca119c508f227f73f4e1dbee344bce8
kamil_pula <sorennx@gmail.com>
2023-11-12 22:52:07 +0100

    Added basic panels

--- apps/back-office/src/routes/admin/company/AdminCompanyRoutes.ts
+++ apps/back-office/src/routes/admin/company/AdminCompanyRoutes.ts
@@ -3,10 +3,7 @@ import BaseEnum from '@sharedEnums/BaseEnum'
 import type { RouteRecordRaw } from 'vue-router'
 
 class AdminProductRoutesEnum extends BaseEnum {
-  static readonly ADMIN_DASHBOARD: string = 'ADMIN_DASHBOARD'
   static readonly ADMIN_COMPANY_TILES: string = 'ADMIN_COMPANY_TILES'
-  static readonly ADMIN_COMPANY_PRODUCT_TILES: string = 'ADMIN_COMPANY_PRODUCT_TILES'
-  static readonly ADMIN_LOCALE_TILES: string = 'ADMIN_LOCALE_TILES'
 }
 
 const AdminCompanyRoutes: RouteRecordRaw[] = [
--- apps/back-office/src/views/Product/AdminProductView.vue
+++ apps/back-office/src/views/Product/AdminProductView.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>AdminLocaleView</div>
+  <a-panel>
+    <a-panel-section>
+      <m-tile-list :tiles="tiles" />
+    </a-panel-section>
+  </a-panel>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouteNames } from '@/routes/index'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-<style scoped lang="scss"></style>
+// Variables
+const { t } = useI18n()
+
+// Computed
+const tiles = computed(() => [
+  {
+    title: t('navigation.tiles.product.verify'),
+    icon: 'verified',
+    to: { name: RouteNames.COMPANY_WAREHOUSE_LIST }
+  },
+  {
+    title: t('navigation.tiles.product.list'),
+    icon: 'list',
+    to: { name: RouteNames.COMPANY_WAREHOUSE_EDITOR }
+  }
+])
+</script>
