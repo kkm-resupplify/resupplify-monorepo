dbe4fc5b56b363adba5093a93421955d1fbf563d
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-11-12 21:43:51 +0100

    add more translations related to adding product

--- apps/portal/src/components/core/company/management/products/editor/form/ProductEditorForm.vue
+++ apps/portal/src/components/core/company/management/products/editor/form/ProductEditorForm.vue
@@ -1,8 +1,10 @@
 <template>
   ProductEditorForm
-  <product-editor-add-information-step />
+  <!-- <product-editor-add-information-step /> -->
+  <product-editor-add-translation-step />
 </template>
 <script setup lang="ts">
-import ProductEditorAddInformationStep from './steps/ProductEditorAddInformationStep.vue'
+// import ProductEditorAddInformationStep from './steps/ProductEditorAddInformationStep.vue'
+import ProductEditorAddTranslationStep from './steps/ProductEditorAddTranslationStep.vue'
 </script>
 <style scoped lang="scss"></style>
