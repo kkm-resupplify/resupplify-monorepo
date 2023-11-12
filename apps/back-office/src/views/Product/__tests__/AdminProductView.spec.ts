10d2250b844d662ebd91d564379c153367068d4c
kamil_pula <sorennx@gmail.com>
2023-11-12 23:36:12 +0100

    Updated BO mock test

---
+++ apps/back-office/src/views/Product/__tests__/AdminProductView.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import AdminProductView from '../AdminProductView.vue'
+import { createTestingPinia } from '@pinia/testing'
+import { useUserStore } from '@/stores/user/useUserStore'
+
+describe('HomeView - Integration', () => {
+  it('renders text properly', async () => {
+    const wrapper = mount(AdminProductView, {
+      global: {
+        plugins: [createTestingPinia()]
+      }
+    })
+    const store = useUserStore()
+    store.email = 'test@pinia.com'
+    // await wrapper.vm.$nextTick()
+
+    // expect(wrapper.text()).toContain('@pinia.com')
+  })
+})
