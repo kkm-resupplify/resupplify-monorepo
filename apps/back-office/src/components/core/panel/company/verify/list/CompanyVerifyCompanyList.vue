f45ac1386b4cb0d80c9d00c6b3eb8ef279cbcfdb
kamil_pula <sorennx@gmail.com>
2023-11-15 21:56:30 +0100

    Added initial components

---
+++ apps/back-office/src/components/core/panel/company/verify/list/CompanyVerifyCompanyList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="company-verify-company-list">
+    <company-verify-company-list-item />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CompanyVerifyCompanyListItem from '@/components/core/panel/company/verify/list/CompanyVerifyCompanyListItem.vue'
+</script>
+
+<style lang="scss" scoped>
+.company-verify-company-list {
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  gap: $global-spacing-30;
+
+  padding: $global-spacing-30;
+}
+</style>
--- apps/back-office/src/components/core/panel/company/verify/section/CompanyVerifyContentSection.vue
+++ apps/back-office/src/components/core/panel/company/verify/section/CompanyVerifyContentSection.vue
@@ -1,7 +1,17 @@
 <template>
-  <a-panel-section> CompanyVerifyContentSection </a-panel-section>
+  <a-panel-section class="company-verify-content-section">
+    <a-title title="Verify companies" />
+
+    <company-verify-company-list />
+  </a-panel-section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CompanyVerifyCompanyList from '@/components/core/panel/company/verify/list/CompanyVerifyCompanyList.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.company-verify-content-section {
+  overflow-y: auto;
+}
+</style>
