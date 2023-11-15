f2e9340c049b9ff727271f9ba3f9c7e06669ede3
kamil_pula <sorennx@gmail.com>
2023-11-15 22:06:12 +0100

    Added initial logic

--- apps/back-office/src/components/core/panel/company/verify/section/CompanyVerifyContentSection.vue
+++ apps/back-office/src/components/core/panel/company/verify/section/CompanyVerifyContentSection.vue
@@ -8,6 +8,17 @@
 
 <script setup lang="ts">
 import CompanyVerifyCompanyList from '@/components/core/panel/company/verify/list/CompanyVerifyCompanyList.vue'
+import CompanyVerificationService from '@/services/company/CompanyVerificationService'
+import { onBeforeMount, ref } from 'vue'
+import type { CompanyData } from '@sharedInterfaces/company/CompanyInterface'
+
+// Variables
+const unverifiedCompanies = ref<CompanyData[]>([])
+
+// Hooks
+onBeforeMount(async () => {
+  unverifiedCompanies.value = await CompanyVerificationService.getUnverifiedCompanies()
+})
 </script>
 
 <style lang="scss" scoped>
