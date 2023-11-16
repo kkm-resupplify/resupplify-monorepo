b20e10b280f457a0adfe093f296c8b7851f1819f
kamil_pula <sorennx@gmail.com>
2023-11-16 21:26:05 +0100

    Added mass verifiation service methods

--- apps/back-office/src/services/company/CompanyVerificationService.ts
+++ apps/back-office/src/services/company/CompanyVerificationService.ts
@@ -1,12 +1,18 @@
+import CompanyStatusEnum from '@sharedEnums/company/CompanyStatusEnum'
 import BaseService from '../BaseService'
 
 interface VerifyCompany {
   companyId: number
 }
 
+interface VerifyAllCompanies {
+  companyIds: number[]
+}
+
 class CompanyVerificationService extends BaseService {
   static VERIFY_COMPANY_SUFFIX = 'verify'
   static REJECT_COMPANY_SUFFIX = 'reject'
+  static MASS_STATUS_UPDATE_SUFFIX = 'massStatusUpdate'
 
   async getUnverifiedCompanies() {
     return this.get({ suffix: CompanyVerificationService.VERIFY_COMPANY_SUFFIX })
@@ -25,6 +31,26 @@ class CompanyVerificationService extends BaseService {
     const response = await this.put({
       suffix: `${CompanyVerificationService.REJECT_COMPANY_SUFFIX}/${companyId}`,
       notificationTitle: 'company.verify.notification.rejectCompanySuccessTitle'
+    })
+
+    return response
+  }
+
+  async verifyAllCompanies({ companyIds }: VerifyAllCompanies) {
+    const response = await this.post({
+      suffix: CompanyVerificationService.MASS_STATUS_UPDATE_SUFFIX,
+      data: { companyIds, newStatus: CompanyStatusEnum.VERIFIED },
+      notificationTitle: 'company.verify.notification.verifyAllCompaniesSuccessTitle'
+    })
+
+    return response
+  }
+
+  async rejectAllCompanies({ companyIds }: VerifyAllCompanies) {
+    const response = await this.post({
+      suffix: CompanyVerificationService.MASS_STATUS_UPDATE_SUFFIX,
+      data: { companyIds, newStatus: CompanyStatusEnum.REJECTED },
+      notificationTitle: 'company.verify.notification.rejectAllCompaniesSuccessTitle'
     })
 
     return response
