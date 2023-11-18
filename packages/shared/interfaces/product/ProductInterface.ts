f64987bed0056a508185f150cf9c342acab41acc
Kamil Pula <kamil.pula.dev@gmail.com>
2023-11-18 13:10:52 +0100

    Added interfaces

--- packages/shared/interfaces/product/ProductInterface.ts
+++ packages/shared/interfaces/product/ProductInterface.ts
@@ -15,9 +15,21 @@ export interface Product {
 export interface ProductSubcategory {
   id: number
   name: string
+  categoryId: number
 }
 
 export interface ProductCategory {
   id: number
   name: string
+}
+
+export interface ProductUnit {
+  id: number
+  name: string
+}
+
+export interface ProductTag {
+  id: number
+  name: string
+  color: string
 }
