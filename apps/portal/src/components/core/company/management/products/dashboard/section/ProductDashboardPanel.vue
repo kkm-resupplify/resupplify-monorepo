7ca3a2ead7749de06565ebbe80c1b4e8821a43eb
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-11-19 21:31:29 +0100

    Added header structure

--- apps/portal/src/components/core/company/management/products/dashboard/section/ProductDashboardPanel.vue
+++ apps/portal/src/components/core/company/management/products/dashboard/section/ProductDashboardPanel.vue
@@ -1,1 +1,13 @@
-<template>Product Dashboard Panel</template>
+<template>
+  <template v-if="isLoading"> implement-loader-here </template>
+
+  <a-panel v-else>
+    <product-header-section />
+  </a-panel>
+</template>
+<script setup lang="ts">
+import ProductHeaderSection from './header/ProductHeaderSection.vue'
+import { ref } from 'vue'
+
+const isLoading = ref(false)
+</script>
---
+++ apps/portal/src/components/core/company/management/products/dashboard/section/header/ProductHeaderSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-panel-section>
+    <div class="product-header-section">
+      <div class="product-header-section__titles">
+        <a-title title="Company name" size="x-large" />
+
+        <a-title title="Total products" size="x-large" />
+      </div>
+
+      <a-button text="Add product" size="x-large" class="product-header-section__button" />
+    </div>
+  </a-panel-section>
+</template>
+<style scoped lang="scss">
+.product-header-section {
+  display: flex;
+  justify-content: space-between;
+  padding: $global-spacing-40;
+}
+</style>
