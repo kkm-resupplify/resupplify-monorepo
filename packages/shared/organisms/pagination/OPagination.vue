a77e495d7888a7a92f6038b91df9a2da804beabc
kamil_pula <sorennx@gmail.com>
2023-11-19 21:39:53 +0100

    Added initial OPagination

---
+++ packages/shared/organisms/pagination/OPagination.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="generateClasses"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useClassComposable } from '@sharedComposables/class/useClassComposable'
+
+const props = defineProps({})
+
+// Variables
+const baseClass = 'o-pagination'
+const { generateClassNames } = useClassComposable()
+
+// Computed
+const generateClasses = computed(() => {
+  return generateClassNames(baseClass, [])
+})
+</script>
+
+<style lang="scss" scoped>
+.o-pagination {
+  display: flex;
+}
+</style>
