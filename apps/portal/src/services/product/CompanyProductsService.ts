33acf432e3ab3f069a31a40623d2424cb3043ba5
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-11-21 23:09:50 +0100

    Added delete method to CompanyProductsService

--- apps/portal/src/services/product/CompanyProductsService.ts
+++ apps/portal/src/services/product/CompanyProductsService.ts
@@ -1,10 +1,20 @@
 import BaseService from '../BaseService'
 
 class CompanyProductsService extends BaseService {
+  static COMPANY_PRODUCTS_SUFFIX = 'product'
+
   async getCompanyProducts(params?: { page?: string; name?: string }) {
     return await this.get({
       config: { params },
       suffix: 'product'
+    })
+  }
+
+  async deleteProduct(id: number) {
+    return await this.delete({
+      id,
+      suffix: CompanyProductsService.COMPANY_PRODUCTS_SUFFIX,
+      notificationTitle: 'company.management.product.editor.deleteSuccessTitle'
     })
   }
 }
