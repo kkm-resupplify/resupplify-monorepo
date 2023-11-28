fb373122f28d4fc87928010484c9d7ecd4fffa09
kamil_pula <sorennx@gmail.com>
2023-11-28 21:46:33 +0100

    Cleaned up store

--- apps/portal/src/stores/product/useProductEditorStore.ts
+++ apps/portal/src/stores/product/useProductEditorStore.ts
@@ -6,33 +6,31 @@ interface ProductTranslation {
   description: string
 }
 
-interface ProductEditorStoreState {
+interface ProductEditorFirstStepData {
   producer: string | null
   code: string | null
   productUnitId: number | null
   productSubcategoryId: number | null
   productTags: number[]
-  translations: ProductTranslation[]
 }
 
-interface ProductEditorFirstStepData {
-  producer: string
-  code: string
-  productUnitId: number
-  productSubcategoryId: number
-  productTags: number[]
+interface ProductEditorStoreState {
+  productEditorFirstStepData: ProductEditorFirstStepData
+  productEditorTranslationStepData: ProductTranslation[]
 }
 
 export const useProductEditorStore = defineStore({
   id: 'productEditor',
 
   state: (): ProductEditorStoreState => ({
-    producer: null,
-    code: null,
-    productUnitId: null,
-    productSubcategoryId: null,
-    productTags: [],
-    translations: []
+    productEditorFirstStepData: {
+      producer: null,
+      code: null,
+      productUnitId: null,
+      productSubcategoryId: null,
+      productTags: []
+    },
+    productEditorTranslationStepData: []
   }),
 
   getters: {
@@ -41,30 +39,12 @@ export const useProductEditorStore = defineStore({
 
   actions: {
     setProductEditorFirstStepData(productEditorFirstStepData: ProductEditorFirstStepData) {
-      this.producer = productEditorFirstStepData.producer
-      this.code = productEditorFirstStepData.code
-      this.productUnitId = productEditorFirstStepData.productUnitId
-      this.productSubcategoryId = productEditorFirstStepData.productSubcategoryId
-      this.productTags = productEditorFirstStepData.productTags
-    },
-
-    addTranslation(productTranslation: ProductTranslation) {
-      const didUpdate = this.updateTranslation(productTranslation)
-      if (!didUpdate) this.translations.push(productTranslation)
-    },
-
-    updateTranslation(productTranslation: ProductTranslation) {
-      const translation = this.translations.find(
-        (oldProductTranslation) =>
-          oldProductTranslation.languageId === productTranslation.languageId
-      )
-
-      if (translation) {
-        Object.assign(translation, productTranslation)
-        return true
-      }
-
-      return false
+      this.productEditorFirstStepData.producer = productEditorFirstStepData.producer
+      this.productEditorFirstStepData.code = productEditorFirstStepData.code
+      this.productEditorFirstStepData.productUnitId = productEditorFirstStepData.productUnitId
+      this.productEditorFirstStepData.productSubcategoryId =
+        productEditorFirstStepData.productSubcategoryId
+      this.productEditorFirstStepData.productTags = productEditorFirstStepData.productTags
     }
   }
 })
