34330d7697110bb76879d1ef73d1904cf5b1d934
kamil_pula <sorennx@gmail.com>
2023-11-30 20:15:07 +0100

    Added initial ProductTag and its list

---
+++ apps/portal/src/components/common/product/ProductTagList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="product-tag-list">
+    <product-tag-list-item
+      v-for="productTag in productTags"
+      :key="productTag.id"
+      :name="productTag.name"
+      :color="productTag.color"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ProductTag } from '@sharedInterfaces/product/ProductTagInterface'
+import { type PropType } from 'vue'
+import ProductTagListItem from '@/components/common/product/ProductTagListItem.vue'
+
+defineProps({
+  productTags: {
+    type: Array as PropType<ProductTag[]>,
+    default: () => []
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.product-tag-list {
+  display: flex;
+  gap: $global-spacing-20;
+}
+</style>
---
+++ apps/portal/src/components/common/product/ProductTagListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <span class="product-tag-list-item" v-text="name" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    default: '#00ff00'
+  }
+})
+
+// Computed
+const colorStyle = computed(() => props.color)
+const backgroundColorStyle = computed(() => `${props.color}10`)
+</script>
+
+<style lang="scss" scoped>
+.product-tag-list-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: fit-content;
+  padding: $global-spacing-10 $global-spacing-30;
+
+  font-size: $global-font-size-30;
+  color: v-bind(colorStyle);
+
+  background-color: v-bind(backgroundColorStyle);
+  border: 1px solid v-bind(colorStyle);
+  border-radius: $global-border-radius-30;
+}
+</style>
