8c83a779795ba9ee9793812404d578071e35c3af
kamil_pula <sorennx@gmail.com>
2023-12-03 00:15:14 +0100

    Updated product tag list item

--- apps/portal/src/components/core/company/management/products/tags/list/ProductTagListItem.vue
+++ apps/portal/src/components/core/company/management/products/tags/list/ProductTagListItem.vue
@@ -1,21 +1,32 @@
 <template>
   <a-list-item-wrapper class="product-tag-list-item">
     <a-list-item-title-section
-      :title="$t('company.management.products.tags.listItem.name')"
+      :title="$t('company.management.products.tags.list.itemName')"
       :value="productTag.name"
-      :basis="20"
     />
+
+    <a-list-item-title-section
+      :title="$t('company.management.products.tags.list.colorValue')"
+      :value="productTag.color"
+    />
+
+    <div class="product-tag-list-item__preview">
+      <a-title :title="$t('global.preview')" />
+
+      <product-tag :product-tag="productTag" />
+    </div>
 
     <update-product-tag-dialog :product-tag="productTag" />
   </a-list-item-wrapper>
 </template>
 <script setup lang="ts">
-import type { ProductTag } from '@sharedInterfaces/product/ProductTagInterface'
+import type { ProductTag as ProductTagInterface } from '@sharedInterfaces/product/ProductTagInterface'
 import UpdateProductTagDialog from '@/components/core/company/management/products/tags/dialog/UpdateProductTagDialog.vue'
+import ProductTag from '@/components/common/product/ProductTag.vue'
 import type { PropType } from 'vue'
 
 defineProps({
-  productTag: { type: Object as PropType<ProductTag>, required: true }
+  productTag: { type: Object as PropType<ProductTagInterface>, required: true }
 })
 </script>
 
@@ -23,5 +34,11 @@ defineProps({
 .product-tag-list-item {
   justify-content: space-between;
   min-width: 600px;
+
+  &__preview {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
