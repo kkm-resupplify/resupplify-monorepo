ae5cf039f6ee654c6355b14b4d4d574584f54e3e
kamil_pula <sorennx@gmail.com>
2023-12-03 01:14:51 +0100

    Added initial product service methods

--- apps/portal/src/services/product/CompanyProductDescriptorsService.ts
+++ apps/portal/src/services/product/CompanyProductDescriptorsService.ts
@@ -1,3 +1,4 @@
+import type { ProductTagData, ProductTag } from '@sharedInterfaces/product/ProductTagInterface'
 import BaseService from '../BaseService'
 
 class CompanyProductDescriptorsService extends BaseService {
@@ -6,6 +7,32 @@ class CompanyProductDescriptorsService extends BaseService {
   async getProductTags() {
     return this.get({
       suffix: CompanyProductDescriptorsService.PRODUCT_TAGS_SUFFIX
+    })
+  }
+
+  async addProductTag(productTagData: ProductTagData) {
+    return this.post({
+      suffix: CompanyProductDescriptorsService.PRODUCT_TAGS_SUFFIX,
+      data: productTagData,
+      notificationTitle: 'company.management.products.tags.notification.addProductTagSuccessTitle'
+    })
+  }
+
+  async deleteProductTag(id: number) {
+    return this.delete({
+      id: id,
+      suffix: CompanyProductDescriptorsService.PRODUCT_TAGS_SUFFIX,
+      notificationTitle:
+        'company.management.products.tags.notification.deleteProductTagSuccessTitle'
+    })
+  }
+
+  async editProductTag(productTagData: ProductTag) {
+    return this.put({
+      id: productTagData.id,
+      suffix: CompanyProductDescriptorsService.PRODUCT_TAGS_SUFFIX,
+      data: productTagData,
+      notificationTitle: 'company.management.products.tags.notification.editProductTagSuccessTitle'
     })
   }
 }
