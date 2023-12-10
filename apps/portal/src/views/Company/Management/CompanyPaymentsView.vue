69e8282452e25a6bc9a2e66b7d8056fe2145ee82
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-10 20:53:47 +0100

    feat: add layout

--- apps/portal/src/views/Company/Management/CompanyPaymentsView.vue
+++ apps/portal/src/views/Company/Management/CompanyPaymentsView.vue
@@ -1,5 +1,30 @@
 <template>
-  <a-panel> Payments </a-panel>
+  <a-panel>
+    <a-panel-section>
+      <m-tile-list :tiles="tiles" />
+    </a-panel-section>
+  </a-panel>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouteNames } from '@/routes/index'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+// Variables
+const { t } = useI18n()
+
+// Computed
+const tiles = computed(() => [
+  {
+    title: t('company.management.navigation.payments.tiles.paymentsDashboard'),
+    icon: 'universal_currency_alt',
+    to: { name: RouteNames.COMPANY_PAYMENTS }
+  },
+  {
+    title: t('company.management.navigation.payments.tiles.manageBalance'),
+    icon: 'account_balance_wallet',
+    to: { name: RouteNames.COMPANY_BALANCE }
+  }
+])
+</script>
