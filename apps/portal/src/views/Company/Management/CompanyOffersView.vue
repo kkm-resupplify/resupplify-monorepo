f8ba62d5769bcb9a44fc4081d716595693bc6e5e
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-10 18:42:04 +0100

    fix: move order tiles from offer view to orders view

--- apps/portal/src/views/Company/Management/CompanyOffersView.vue
+++ apps/portal/src/views/Company/Management/CompanyOffersView.vue
@@ -1,30 +1,3 @@
 <template>
-  <a-panel>
-    <a-panel-section>
-      <m-tile-list :tiles="tiles" />
-    </a-panel-section>
-  </a-panel>
+  <a-panel> Company Offers View </a-panel>
 </template>
-
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { RouteNames } from '@/routes/index'
-
-// Variables
-const { t } = useI18n()
-
-// Computed
-const tiles = computed(() => [
-  {
-    title: t('company.management.navigation.offers.tiles.ourPurchases'),
-    icon: 'shopping_cart',
-    to: { name: RouteNames.COMPANY_PRODUCT_LIST }
-  },
-  {
-    title: t('company.management.navigation.offers.tiles.customerOrders'),
-    icon: 'order_approve',
-    to: { name: RouteNames.COMPANY_PRODUCT_EDITOR }
-  }
-])
-</script>
