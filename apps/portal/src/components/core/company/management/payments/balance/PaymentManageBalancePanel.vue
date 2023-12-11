47a27e2fa2204dff7fcb98112b8c5f34292eb181
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-11 21:12:53 +0100

    feat: add fields

--- apps/portal/src/components/core/company/management/payments/balance/PaymentManageBalancePanel.vue
+++ apps/portal/src/components/core/company/management/payments/balance/PaymentManageBalancePanel.vue
@@ -14,14 +14,44 @@
         size="large"
       />
     </div>
+
+    <div class="payment-manage-balance-panel__content">
+      <m-select
+        name="type"
+        rules="required"
+        :label="$t('company.management.navigation.payments.manageBalance.operationTypeLabel')"
+        :placeholder="
+          $t('company.management.navigation.payments.manageBalance.operationTypePlaceholder')
+        "
+        :options="operationTypes"
+      />
+
+      <m-text-field
+        name="amount"
+        input-type="number"
+        rules="required"
+        :label="$t('company.management.navigation.payments.manageBalance.amountLabel')"
+        :placeholder="$t('company.management.navigation.payments.manageBalance.amountPlaceholder')"
+        :options="operationTypes"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+// Variables
 const balance = ref(1410)
+const { t } = useI18n()
 
+const operationTypes = ref([
+  { id: 0, text: t('company.management.navigation.payments.manageBalance.deposit') },
+  { id: 1, text: t('company.management.navigation.payments.manageBalance.withdraw') }
+])
+
+// Computed
 const formattedBalance = computed(() => {
   return `: $${balance.value}`
 })
@@ -29,16 +59,25 @@ const formattedBalance = computed(() => {
 
 <style scoped lang="scss">
 .payment-manage-balance-panel {
+  display: flex;
+  flex-direction: column;
+  gap: $global-spacing-100;
   padding: $global-spacing-100;
 
   &__header {
     display: flex;
     flex-direction: column;
-    gap: $global-spacing-70;
+    gap: $global-spacing-40;
   }
 
   &__current-balance {
     white-space: nowrap;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-60;
   }
 }
 </style>
