7de7f00a7a0a989550afafcffb26ff9f83aa2899
kamil_pula <sorennx@gmail.com>
2023-12-11 19:16:44 +0100

    Added props for payments and isLoading

--- apps/portal/src/components/core/company/management/payments/dashboard/PaymentDashboardPanel.vue
+++ apps/portal/src/components/core/company/management/payments/dashboard/PaymentDashboardPanel.vue
@@ -2,11 +2,17 @@
   <a-panel>
     <payment-dashboard-header-section />
 
-    <payment-dashboard-content-section />
+    <payment-dashboard-content-section :payments="payments" :is-loading="isLoading" />
   </a-panel>
 </template>
 
 <script setup lang="ts">
 import PaymentDashboardHeaderSection from '@/components/core/company/management/payments/dashboard/section/header/PaymentDashboardHeaderSection.vue'
 import PaymentDashboardContentSection from '@/components/core/company/management/payments/dashboard/section/content/PaymentDashboardContentSection.vue'
+import { ref } from 'vue'
+import type { Payment } from '@sharedInterfaces/balance/CompanyBalanceInterface'
+
+// Variables
+const payments = ref<Payment[]>([])
+const isLoading = ref(false)
 </script>
