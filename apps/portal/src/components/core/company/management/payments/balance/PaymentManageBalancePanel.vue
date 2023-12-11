0a0fdba15ce56db50a8e098a567a990c9ab828fd
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-11 20:41:07 +0100

    feat: add header

--- apps/portal/src/components/core/company/management/payments/balance/PaymentManageBalancePanel.vue
+++ apps/portal/src/components/core/company/management/payments/balance/PaymentManageBalancePanel.vue
@@ -1,1 +1,44 @@
-<template>Payment Manage Balance Panel</template>
+<template>
+  <div class="payment-manage-balance-panel">
+    <div class="payment-manage-balance-panel__header">
+      <a-title
+        :title="$t('company.management.navigation.payments.manageBalance.title')"
+        size="x-large"
+      />
+
+      <a-title
+        :title="$t('company.management.navigation.payments.manageBalance.currentBalance')"
+        :subtitle="formattedBalance"
+        variant="horizontal"
+        class="payment-manage-balance-panel__current-balance"
+        size="large"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const balance = ref(1410)
+
+const formattedBalance = computed(() => {
+  return `: $${balance.value}`
+})
+</script>
+
+<style scoped lang="scss">
+.payment-manage-balance-panel {
+  padding: $global-spacing-100;
+
+  &__header {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-70;
+  }
+
+  &__current-balance {
+    white-space: nowrap;
+  }
+}
+</style>
