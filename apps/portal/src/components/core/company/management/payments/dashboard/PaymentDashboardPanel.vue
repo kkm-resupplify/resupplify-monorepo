5e8d3c663488267e7c63cdbba099f5cf64130b48
kamil_pula <sorennx@gmail.com>
2023-12-11 20:37:45 +0100

    Added mock data

--- apps/portal/src/components/core/company/management/payments/dashboard/PaymentDashboardPanel.vue
+++ apps/portal/src/components/core/company/management/payments/dashboard/PaymentDashboardPanel.vue
@@ -13,6 +13,79 @@ import { ref } from 'vue'
 import type { Payment } from '@sharedInterfaces/balance/CompanyBalanceInterface'
 
 // Variables
-const payments = ref<Payment[]>([])
+const payments = ref<Payment[]>([
+  {
+    id: 1,
+    amount: 189.82,
+    currency: 'EURO',
+    type: 1,
+    status: 1,
+    date: new Date(),
+    sender: {
+      name: 'Sender Company Inc.',
+      email: 'sender-company@gmail.com',
+      contactPerson: 'John Senderson'
+    },
+    receiver: {
+      name: 'Receiver Company Inc.',
+      email: 'receiver-company@gmail.com',
+      contactPerson: 'Juliet Receiverson'
+    }
+  },
+  {
+    id: 2,
+    amount: 212.02,
+    currency: 'EURO',
+    type: 2,
+    status: 1,
+    date: new Date(),
+    sender: {
+      name: 'Sender Company Inc.',
+      email: 'sender-company@gmail.com',
+      contactPerson: 'John Senderson'
+    },
+    receiver: {
+      name: 'Receiver Company Inc.',
+      email: 'receiver-company@gmail.com',
+      contactPerson: 'Juliet Receiverson'
+    }
+  },
+  {
+    id: 3,
+    amount: 583.58,
+    currency: 'EURO',
+    type: 3,
+    status: 1,
+    date: new Date(),
+    sender: {
+      name: 'Sender Company Inc.',
+      email: 'sender-company@gmail.com',
+      contactPerson: 'John Senderson'
+    },
+    receiver: {
+      name: 'Receiver Company Inc.',
+      email: 'receiver-company@gmail.com',
+      contactPerson: 'Juliet Receiverson'
+    }
+  },
+  {
+    id: 4,
+    amount: 483.3,
+    currency: 'EURO',
+    type: 4,
+    status: 1,
+    date: new Date(),
+    sender: {
+      name: 'Sender Company Inc.',
+      email: 'sender-company@gmail.com',
+      contactPerson: 'John Senderson'
+    },
+    receiver: {
+      name: 'Receiver Company Inc.',
+      email: 'receiver-company@gmail.com',
+      contactPerson: 'Juliet Receiverson'
+    }
+  }
+])
 const isLoading = ref(false)
 </script>
