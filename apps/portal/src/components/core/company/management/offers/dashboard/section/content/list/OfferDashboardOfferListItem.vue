cd10ceee1e0125d88e080440710a301ea4e2c163
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-13 00:17:32 +0100

    feat: add template structure

--- apps/portal/src/components/core/company/management/offers/dashboard/section/content/list/OfferDashboardOfferListItem.vue
+++ apps/portal/src/components/core/company/management/offers/dashboard/section/content/list/OfferDashboardOfferListItem.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>OfferDashboardOfferListItem</div>
+  <a-expansion-panel class="offer-dashboard-offer-list-item">
+    <template #activator>
+      <a-list-item-wrapper class="offer-dashboard-offer-list-item__activator">
+      </a-list-item-wrapper>
+    </template>
+
+    <template #content>
+      <div class="offer-dashboard-offer-list-item__content"></div>
+    </template>
+  </a-expansion-panel>
 </template>
 
 <script setup lang="ts"></script>
