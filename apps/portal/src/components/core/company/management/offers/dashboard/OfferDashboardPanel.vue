e6f0a17514c54d00bbc2246b8e96711479448b71
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-12 19:20:52 +0100

    feat: implement props structure

--- apps/portal/src/components/core/company/management/offers/dashboard/OfferDashboardPanel.vue
+++ apps/portal/src/components/core/company/management/offers/dashboard/OfferDashboardPanel.vue
@@ -2,11 +2,17 @@
   <a-panel>
     <offer-dashboard-header-section />
 
-    <offer-dashboard-content-section />
+    <offer-dashboard-content-section :offers="offers" :is-loading="isLoading" />
   </a-panel>
 </template>
 
 <script setup lang="ts">
 import OfferDashboardHeaderSection from '@/components/core/company/management/offers/dashboard/section/header/OfferDashboardHeaderSection.vue'
 import OfferDashboardContentSection from '@/components/core/company/management/offers/dashboard/section/content/OfferDashboardContentSection.vue'
+import { ref } from 'vue'
+import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+
+// Variables
+const offers = ref<Offer[]>([])
+const isLoading = ref(false)
 </script>
