e85561821c757aef45d889fd28055764dd395874
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-13 14:50:17 +0100

    feat: add section dto with translations dividen by activator and content section

--- apps/portal/src/components/core/company/management/offers/dashboard/section/content/list/OfferDashboardOfferListItem.vue
+++ apps/portal/src/components/core/company/management/offers/dashboard/section/content/list/OfferDashboardOfferListItem.vue
@@ -14,10 +14,49 @@
 <script setup lang="ts">
 import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
 import type { PropType } from 'vue'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-defineProps({
+const props = defineProps({
   offer: {
-    type: Object as PropType<Offer>
+    type: Object as PropType<Offer>,
+    required: true
   }
+})
+
+// DTO
+class ActivatorSectionDto {
+  title: string
+  value: string | number
+
+  constructor(title: string, value: string | number) {
+    this.title = this.getTranslationValue(title)
+    this.value = value
+  }
+
+  getTranslationValue(key: string): string {
+    return t(`company.management.balance.dashboard.offer.section.${key}`)
+  }
+}
+
+// Variables
+const { t } = useI18n()
+
+// Computed
+const activatorSections = computed(() => {
+  return [
+    new ActivatorSectionDto('netPrice', props.offer.product.netPrice),
+    new ActivatorSectionDto('unit', props.offer.product.productUnit.code),
+    new ActivatorSectionDto('supply', props.offer.product.supply),
+    new ActivatorSectionDto('quantity', props.offer.product.quantity)
+  ]
+})
+
+const contentSectionsTitles = computed(() => {
+  return [
+    new ActivatorSectionDto('productCode', props.offer.product.code),
+    new ActivatorSectionDto('productDescription', props.offer.product.description),
+    new ActivatorSectionDto('productProducer', props.offer.product.producer)
+  ]
 })
 </script>
