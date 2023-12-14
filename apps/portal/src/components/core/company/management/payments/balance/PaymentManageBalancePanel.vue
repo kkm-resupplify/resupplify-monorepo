d2aa1720b90a13e9cf2e30f60eb62a4666fa1594
kamil_pula <sorennx@gmail.com>
2023-12-15 00:57:54 +0100

    Updated structure & style

--- apps/portal/src/components/core/company/management/payments/balance/PaymentManageBalancePanel.vue
+++ apps/portal/src/components/core/company/management/payments/balance/PaymentManageBalancePanel.vue
@@ -1,50 +1,59 @@
 <template>
-  <div class="payment-manage-balance-panel">
-    <div class="payment-manage-balance-panel__header">
-      <a-title :title="$t('company.management.balance.manage.title')" size="x-large" />
+  <a-panel class="payment-manage-balance-panel">
+    <a-panel-section>
+      <div class="payment-manage-balance-panel__header">
+        <a-title :title="$t('company.management.balance.manage.title')" size="x-large" />
 
-      <a-title
-        :title="$t('company.management.balance.manage.currentBalance')"
-        :subtitle="formattedBalance"
-        variant="horizontal"
-        class="payment-manage-balance-panel__current-balance"
-        size="large"
-        append-colon
-      />
-    </div>
+        <a-title
+          :title="$t('company.management.balance.manage.currentBalance')"
+          :subtitle="formattedBalance"
+          variant="horizontal"
+          class="payment-manage-balance-panel__current-balance"
+          size="large"
+          append-colon
+        />
+      </div>
 
-    <o-form :submit-callback="handleFormSubmit" class="payment-manage-balance-panel__form">
-      <template #body>
-        <div class="payment-manage-balance-panel__form-content">
-          <m-select
-            name="type"
-            rules="required"
-            :label="$t('company.management.balance.manage.operationTypeLabel')"
-            :placeholder="$t('company.management.balance.manage.operationTypePlaceholder')"
-            :options="operationTypes"
-            @input-change="handleSelectOperationTypeChange"
+      <o-form :submit-callback="handleFormSubmit" class="payment-manage-balance-panel__form">
+        <template #body>
+          <div class="payment-manage-balance-panel__form-content">
+            <m-select
+              name="type"
+              rules="required"
+              :label="$t('company.management.balance.manage.operationTypeLabel')"
+              :placeholder="$t('company.management.balance.manage.operationTypePlaceholder')"
+              :options="operationTypes"
+              @input-change="handleSelectOperationTypeChange"
+            />
+
+            <m-text-field
+              name="amount"
+              input-type="number"
+              rules="required"
+              :label="$t('company.management.balance.manage.amountLabel')"
+              :placeholder="$t('company.management.balance.manage.amountPlaceholder')"
+              :options="operationTypes"
+            />
+          </div>
+        </template>
+        <template #footer>
+          <a-button
+            button-type="submit"
+            :text="submitButtonText"
+            size="x-large"
+            style="width: 100%"
           />
-
-          <m-text-field
-            name="amount"
-            input-type="number"
-            rules="required"
-            :label="$t('company.management.balance.manage.amountLabel')"
-            :placeholder="$t('company.management.balance.manage.amountPlaceholder')"
-            :options="operationTypes"
-          />
-        </div>
-      </template>
-      <template #footer>
-        <a-button button-type="submit" :text="submitButtonText" size="x-large" />
-      </template>
-    </o-form>
-  </div>
+        </template>
+      </o-form>
+    </a-panel-section>
+  </a-panel>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
+import CompanyBalanceService from '@/services/company/CompanyBalanceService'
+import PaymentTypeEnum from '@sharedEnums/payment/PaymentTypeEnum'
 
 // Interfaces
 interface FormData {
@@ -53,24 +62,27 @@ interface FormData {
 }
 
 // Variables
-const balance = ref(1410)
+const isLoading = ref(false)
+const balance = ref(0)
 const { t } = useI18n()
 const selectedOperationTypeId = ref<number | null>(null)
 
-const operationTypes = ref([
-  { id: 1, text: t('company.management.balance.manage.withdraw') },
-  { id: 2, text: t('company.management.balance.manage.deposit') }
+// Computed
+const operationTypes = computed(() => [
+  { id: PaymentTypeEnum.WITHDRAWAL, text: t('company.management.balance.manage.withdraw') },
+  { id: PaymentTypeEnum.DEPOSIT, text: t('company.management.balance.manage.deposit') }
 ])
 
-// Computed
 const formattedBalance = computed(() => {
   return `€${balance.value}`
 })
 
 const submitButtonText = computed(() => {
-  if (selectedOperationTypeId.value === 0) return t('company.management.balance.manage.deposit')
+  if (selectedOperationTypeId.value === PaymentTypeEnum.DEPOSIT)
+    return t('company.management.balance.manage.deposit')
 
-  if (selectedOperationTypeId.value === 1) return t('company.management.balance.manage.withdraw')
+  if (selectedOperationTypeId.value === PaymentTypeEnum.WITHDRAWAL)
+    return t('company.management.balance.manage.withdraw')
 
   return t('company.management.balance.manage.selectOperationType')
 })
@@ -80,8 +92,18 @@ const handleSelectOperationTypeChange = (id: number) => {
   selectedOperationTypeId.value = id
 }
 
-const handleFormSubmit = (data: FormData) => {
-  console.log(data)
+const handleFormSubmit = async (data: FormData) => {
+  isLoading.value = true
+
+  if (selectedOperationTypeId.value === PaymentTypeEnum.WITHDRAWAL) {
+    await CompanyBalanceService.withdrawBalance(data.amount)
+  }
+
+  if (selectedOperationTypeId.value === PaymentTypeEnum.DEPOSIT) {
+    await CompanyBalanceService.depositBalance(data.amount)
+  }
+
+  isLoading.value = false
 }
 </script>
 
@@ -96,6 +118,7 @@ const handleFormSubmit = (data: FormData) => {
     display: flex;
     flex-direction: column;
     gap: $global-spacing-40;
+    max-width: 400px;
   }
 
   &__current-balance {
@@ -103,6 +126,7 @@ const handleFormSubmit = (data: FormData) => {
   }
 
   &__form {
+    max-width: 400px;
     max-height: 190px;
   }
 
