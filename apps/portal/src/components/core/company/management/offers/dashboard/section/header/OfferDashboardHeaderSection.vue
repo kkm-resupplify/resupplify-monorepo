5ccab001cd9eb26d891066988532f4557aecab01
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-14 21:50:32 +0100

    feat: add layout

--- apps/portal/src/components/core/company/management/offers/dashboard/section/header/OfferDashboardHeaderSection.vue
+++ apps/portal/src/components/core/company/management/offers/dashboard/section/header/OfferDashboardHeaderSection.vue
@@ -1,6 +1,10 @@
 <template>
   <a-panel-section class="offer-dashboard-header-section">
-    offer-dashboard-header-section
+    <a-title :title="$t('company.management.offer.companyOffers')" size="x-large" />
+
+    <m-alert variant="danger">
+      <span v-text="$t('company.management.offer.companyOffersNotVisible')" />
+    </m-alert>
   </a-panel-section>
 </template>
 
