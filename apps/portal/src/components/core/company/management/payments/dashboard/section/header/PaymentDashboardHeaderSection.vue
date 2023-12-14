ca15c719f1b6e8aca5dd8e9d8b877bd75d3ff4b9
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-15 00:38:54 +0100

    feat: add layout

--- apps/portal/src/components/core/company/management/payments/dashboard/section/header/PaymentDashboardHeaderSection.vue
+++ apps/portal/src/components/core/company/management/payments/dashboard/section/header/PaymentDashboardHeaderSection.vue
@@ -1,13 +1,42 @@
 <template>
   <a-panel-section class="payment-dashboard-header-section">
-    payment-dashboard-header-section
+    <div class="payment-dashboard-header-section__wrapper">
+      <a-title
+        :title="$t('company.management.balance.dashboard.companyBalance')"
+        :subtitle="formattedBalance"
+        size="x-large"
+      />
+
+      <a-button
+        :text="$t('global.manage')"
+        size="x-large"
+        class="payment-dashboard-header-section__manage"
+      />
+    </div>
   </a-panel-section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+// Variables
+const balance = ref(999999)
+
+// Computed
+const formattedBalance = computed(() => {
+  return `${balance.value}$`
+})
+</script>
 
 <style lang="scss" scoped>
 .payment-dashboard-header-section {
-  display: flex;
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__manage {
+    height: fit-content;
+  }
 }
 </style>
