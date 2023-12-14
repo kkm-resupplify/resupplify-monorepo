974f508054648e1d386182c85510f88a2f582635
kamil_pula <sorennx@gmail.com>
2023-12-15 00:57:44 +0100

    Updated service

--- apps/portal/src/services/company/CompanyBalanceService.ts
+++ apps/portal/src/services/company/CompanyBalanceService.ts
@@ -1,3 +1,5 @@
+import PaymentMethodEnum from '@sharedEnums/payment/PaymentMethodEnum'
+import PaymentTypeEnum from '@sharedEnums/payment/PaymentTypeEnum'
 import BaseService from '../BaseService'
 
 class CompanyBalanceService extends BaseService {
@@ -11,6 +13,30 @@ class CompanyBalanceService extends BaseService {
     return await this.get({
       suffix: CompanyBalanceService.TRANSACTIONS_SUFFIX,
       config: { params }
+    })
+  }
+
+  async withdrawBalance(amount: string) {
+    return await this.post({
+      data: {
+        type: PaymentTypeEnum.WITHDRAWAL,
+        amount,
+        status: 1,
+        currency: 'Euro',
+        paymentMethodId: PaymentMethodEnum.BALANCE
+      }
+    })
+  }
+
+  async depositBalance(amount: string) {
+    return await this.post({
+      data: {
+        type: PaymentTypeEnum.DEPOSIT,
+        amount,
+        status: 1,
+        currency: 'Euro',
+        paymentMethodId: PaymentMethodEnum.BALANCE
+      }
     })
   }
 }
