ec987443ca65864ab78196a51582ad2765a81753
kamil_pula <sorennx@gmail.com>
2023-12-16 17:38:52 +0100

    Added initial ACurrency

---
+++ packages/shared/atoms/currency/ACurrency.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="generateClasses">
+    <a-title :size="size" :title="title" />
+
+    <i18n-n tag="span" :value="value" format="currency" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useClassComposable } from '@sharedComposables/class/useClassComposable'
+
+const props = defineProps({
+  title: String,
+  value: {
+    type: Number,
+    required: true
+  },
+  size: String
+})
+
+// Variables
+const baseClass = 'a-currency'
+const { generateClassNames } = useClassComposable()
+
+// Computed
+const generateClasses = computed(() => {
+  return generateClassNames(baseClass, [])
+})
+</script>
+
+<style lang="scss" scoped>
+.a-currency {
+  display: flex;
+}
+</style>
