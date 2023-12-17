370f17efd0f5a362ba1fbb452dea04ce3f3bdac3
kamil_pula <sorennx@gmail.com>
2023-12-17 01:36:09 +0100

    Added new views

--- apps/portal/src/views/Companies/CompaniesView.vue
+++ apps/portal/src/views/Companies/CompaniesView.vue
@@ -1,14 +1,11 @@
 <template>
   <basic-view-layout>
     <template #body>
-      <browse-dashboard />
+      <div>companies-view</div>
     </template>
   </basic-view-layout>
 </template>
 
 <script setup lang="ts">
-import BrowseDashboard from '@/components/core/browse/BrowseDashboard.vue'
 import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
 </script>
-
-<!-- <style scoped lang="scss"></style> -->
---
+++ apps/portal/src/views/Offers/OffersView.vue
@@ -0,0 +1,11 @@
+<template>
+  <basic-view-layout>
+    <template #body>
+      <div>offers-view</div>
+    </template>
+  </basic-view-layout>
+</template>
+
+<script setup lang="ts">
+import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
+</script>
