23d52b3bd9c77cdc472e5610dd23f24c820917aa
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-17 23:27:54 +0100

    feat: define structure of props

--- apps/portal/src/components/core/company/preview/offer/dashboard/section/content/CompanyPreviewContentSection.vue
+++ apps/portal/src/components/core/company/preview/offer/dashboard/section/content/CompanyPreviewContentSection.vue
@@ -3,10 +3,13 @@
 
   <br />
 
-  <company-preview-offer-list />
+  <company-preview-offer-list :offers="offers" />
 </template>
 
 <script setup lang="ts">
 import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
 import CompanyPreviewOfferList from './list/CompanyPreviewOfferList.vue'
+import { ref } from 'vue'
+
+const offers = ref<Offer>([])
 </script>
