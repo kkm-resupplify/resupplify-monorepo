74a5bb029f0d3a67ffc2af8a5c1a7eed0eac7b39
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-17 18:19:35 +0100

    feat: add getting product categories with subcategories

--- apps/portal/src/components/common/offer/OfferFilters.vue
+++ apps/portal/src/components/common/offer/OfferFilters.vue
@@ -11,16 +11,92 @@
     <m-select
       name="categoryId"
       :placeholder="$t('offers.filters.categoryPlaceholder')"
+      :options="staticProductDescriptorsStore.getProductCategories"
       :validate="false"
+      @input-change="handleProductCategoryChange"
     />
 
     <m-select
+      ref="subcategoryRef"
       name="subcategoryId"
       :placeholder="$t('offers.filters.subcategoryPlaceholder')"
+      :options="productCategorySubcategories"
       :validate="false"
     />
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import MSelect from '@sharedMolecules/select/MSelect.vue'
+import { useStaticProductDescriptorsStore } from '@sharedStores/product/useStaticProductDescriptorsStore'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import type { Product } from '@sharedInterfaces/product/ProductInterface'
+import type { Pagination } from '@sharedInterfaces/config/PaginationInterface'
+import CompanyProductsService from '@/services/product/CompanyProductsService'
+import StaticProductDescriptorsService from '@/services/product/StaticProductDescriptorsService'
+import { onBeforeMount } from 'vue'
+
+// Emits
+const emits = defineEmits(['update-stats'])
+
+// Variables
+const products = ref<Product[]>([])
+const subcategoryRef = ref<typeof MSelect>()
+const selectedCategoryId = ref<number | null>()
+const staticProductDescriptorsStore = useStaticProductDescriptorsStore()
+const route = useRoute()
+const isLoading = ref(false)
+const paginationData = ref<Pagination>()
+
+// Computed
+const productCategorySubcategories = computed(() => {
+  return selectedCategoryId.value
+    ? staticProductDescriptorsStore.getProductSubcategories.filter(
+        (subcategory) => subcategory.categoryId === selectedCategoryId.value
+      )
+    : staticProductDescriptorsStore.getProductSubcategories
+})
+
+// Methods
+const handleProductCategoryChange = (id: number) => {
+  subcategoryRef?.value?.clearSelect()
+  selectedCategoryId.value = id
+}
+
+const handleFetchProducts = async () => {
+  isLoading.value = true
+
+  const {
+    query: { page, name, categoryId, subcategoryId, status, verificationStatus }
+  } = route
+
+  const { data, success, pagination } = await CompanyProductsService.getProducts({
+    page: page as string,
+    name: name as string,
+    categoryId: categoryId as string,
+    subcategoryId: subcategoryId as string,
+    status: status as string,
+    verificationStatus: verificationStatus as string
+  })
+
+  if (success) {
+    products.value = data
+    paginationData.value = pagination
+    emits('update-stats')
+  }
+
+  isLoading.value = false
+}
+
+// Hooks
+onBeforeMount(async () => {
+  await handleFetchProducts()
+  await StaticProductDescriptorsService.getCategories()
+  await StaticProductDescriptorsService.getSubcategories()
+})
+</script>
 
 <style scoped lang="scss">
 .offers-filters {
