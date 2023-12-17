4111ed454aa38e7faa9d5aa1d8e2c4fa6ba3ba02
kamil_pula <sorennx@gmail.com>
2023-12-17 02:05:29 +0100

    Added basic OfferList and OfferListItem

---
+++ apps/portal/src/components/common/offer/OfferList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="offer-list">
+    <offer-list-item v-for="offer in offers" :key="offer.id" :offer="offer" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { type PropType } from 'vue'
+import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import OfferListItem from '@/components/common/offer/OfferListItem.vue'
+
+defineProps({
+  offers: {
+    type: Array as PropType<Offer[]>,
+    required: true
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.offer-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ apps/portal/src/components/common/offer/OfferListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-list-item-wrapper class="offer-list-item">
+    {{ offer }}
+  </a-list-item-wrapper>
+</template>
+
+<script setup lang="ts">
+import { type PropType } from 'vue'
+import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+
+defineProps({
+  offer: {
+    type: Object as PropType<Offer>,
+    required: true
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.offer-list-item {
+  display: flex;
+  flex-direction: column;
+}
+</style>
