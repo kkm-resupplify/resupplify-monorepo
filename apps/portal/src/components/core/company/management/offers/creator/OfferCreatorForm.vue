ad13b0c0fc78437d2f7cd478c368a0724d2b2236
kamil_pula <sorennx@gmail.com>
2023-12-17 22:19:46 +0100

    Updated translations, expanded form

--- apps/portal/src/components/core/company/management/offers/creator/OfferCreatorForm.vue
+++ apps/portal/src/components/core/company/management/offers/creator/OfferCreatorForm.vue
@@ -7,24 +7,40 @@
         <m-select
           name="product"
           :options="selectProductOptions"
-          :label="$t('company.management.offer.creator.selectProductLabel')"
-          :placeholder="$t('company.management.offer.creator.selectProductPlaceholder')"
+          :label="$t('company.management.offer.creator.selectStockItemLabel')"
+          :placeholder="$t('company.management.offer.creator.selectStockItemPlaceholder')"
           rules="required"
         />
 
         <m-text-field
           name="quantity"
           input-type="number"
-          :label="$t('company.management.offer.creator.selectQuantityLabel')"
-          :placeholder="$t('company.management.offer.creator.selectQuantityPlaceholder')"
+          :label="$t('company.management.offer.creator.enterQuantityLabel')"
+          :placeholder="$t('company.management.offer.creator.enterQuantityPlaceholder')"
           rules="required"
         />
 
         <m-text-field
           name="netPrice"
           input-type="number"
-          :label="$t('company.management.offer.creator.selectNetPriceLabel')"
-          :placeholder="$t('company.management.offer.creator.selectNetPricePlaceholder')"
+          :label="$t('company.management.offer.creator.enterNetPriceLabel')"
+          :placeholder="$t('company.management.offer.creator.enterNetPricePlaceholder')"
+          rules="required"
+        />
+
+        <m-text-field
+          name="dateStart"
+          input-type="date"
+          :label="$t('company.management.offer.creator.enterStartDateLabel')"
+          :placeholder="$t('company.management.offer.creator.enterStartDatePlaceholder')"
+          rules="required"
+        />
+
+        <m-text-field
+          name="dateStart"
+          input-type="date"
+          :label="$t('company.management.offer.creator.enterEndDateLabel')"
+          :placeholder="$t('company.management.offer.creator.enterEndDatePlaceholder')"
           rules="required"
         />
       </div>
@@ -44,8 +60,7 @@
 <script setup lang="ts">
 import CompanyProductsService from '@/services/product/CompanyProductsService'
 import type { Product } from '@sharedInterfaces/product/ProductInterface'
-import { computed } from 'vue'
-import { ref, onBeforeMount } from 'vue'
+import { ref, onBeforeMount, computed } from 'vue'
 
 // Variables
 const products = ref<Product[]>([])
