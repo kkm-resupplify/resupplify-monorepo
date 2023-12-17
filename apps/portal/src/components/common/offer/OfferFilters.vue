3ac931e197f4148e53036e76708349bab17c6f98
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-17 18:37:05 +0100

    fix: change way of sending data to not include url params

--- apps/portal/src/components/common/offer/OfferFilters.vue
+++ apps/portal/src/components/common/offer/OfferFilters.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="offers-filters">
-    <m-text-field
-      class="offer-filters__name-search"
-      name="name"
-      :placeholder="$t('offers.filters.namePlaceholder')"
-      :validate="false"
-      append-icon-on="close"
-    />
+  <o-form ref="form" class="offer-filters" :submit-callback="handleQuerySubmit">
+    <template #body>
+      <div class="offer-filters__inputs">
+        <m-text-field
+          name="name"
+          :placeholder="$t('offers.filters.namePlaceholder')"
+          :validate="false"
+          append-icon-on="close"
+        />
 
-    <m-select
-      name="categoryId"
-      :placeholder="$t('offers.filters.categoryPlaceholder')"
-      :options="staticProductDescriptorsStore.getProductCategories"
-      :validate="false"
-      @input-change="handleProductCategoryChange"
-    />
+        <m-select
+          name="categoryId"
+          :placeholder="$t('offers.filters.categoryPlaceholder')"
+          :options="staticProductDescriptorsStore.getProductCategories"
+          :validate="false"
+          @input-change="handleProductCategoryChange"
+        />
 
-    <m-select
-      ref="subcategoryRef"
-      name="subcategoryId"
-      :placeholder="$t('offers.filters.subcategoryPlaceholder')"
-      :options="productCategorySubcategories"
-      :validate="false"
-    />
-  </div>
+        <m-select
+          ref="subcategoryRef"
+          name="subcategoryId"
+          :placeholder="$t('offers.filters.subcategoryPlaceholder')"
+          :options="productCategorySubcategories"
+          :validate="false"
+        />
+      </div>
+    </template>
+    <template #footer>
+      <a-button button-type="submit" text="search" size="x-large" />
+    </template>
+  </o-form>
 </template>
 
 <script setup lang="ts">
@@ -31,24 +37,27 @@ import { ref } from 'vue'
 import MSelect from '@sharedMolecules/select/MSelect.vue'
 import { useStaticProductDescriptorsStore } from '@sharedStores/product/useStaticProductDescriptorsStore'
 import { computed } from 'vue'
-import { useRoute } from 'vue-router'
 import type { Product } from '@sharedInterfaces/product/ProductInterface'
-import type { Pagination } from '@sharedInterfaces/config/PaginationInterface'
 import CompanyProductsService from '@/services/product/CompanyProductsService'
 import StaticProductDescriptorsService from '@/services/product/StaticProductDescriptorsService'
 import { onBeforeMount } from 'vue'
 
+// Interfaces
+interface filterData {
+  name: string
+  categoryId: number
+  subcategoryId: number
+}
+
 // Emits
-const emits = defineEmits(['update-stats'])
+const emits = defineEmits(['filter'])
 
 // Variables
 const products = ref<Product[]>([])
 const subcategoryRef = ref<typeof MSelect>()
 const selectedCategoryId = ref<number | null>()
 const staticProductDescriptorsStore = useStaticProductDescriptorsStore()
-const route = useRoute()
 const isLoading = ref(false)
-const paginationData = ref<Pagination>()
 
 // Computed
 const productCategorySubcategories = computed(() => {
@@ -68,26 +77,17 @@ const handleProductCategoryChange = (id: number) => {
 const handleFetchProducts = async () => {
   isLoading.value = true
 
-  const {
-    query: { page, name, categoryId, subcategoryId, status, verificationStatus }
-  } = route
-
-  const { data, success, pagination } = await CompanyProductsService.getProducts({
-    page: page as string,
-    name: name as string,
-    categoryId: categoryId as string,
-    subcategoryId: subcategoryId as string,
-    status: status as string,
-    verificationStatus: verificationStatus as string
-  })
+  const { data, success } = await CompanyProductsService.getProducts()
 
   if (success) {
     products.value = data
-    paginationData.value = pagination
-    emits('update-stats')
   }
 
   isLoading.value = false
+}
+
+const handleQuerySubmit = async (data: filterData) => {
+  console.log(data)
 }
 
 // Hooks
@@ -99,7 +99,9 @@ onBeforeMount(async () => {
 </script>
 
 <style scoped lang="scss">
-.offers-filters {
-  display: flex;
+.offer-filters {
+  &__inputs {
+    display: flex;
+  }
 }
 </style>
