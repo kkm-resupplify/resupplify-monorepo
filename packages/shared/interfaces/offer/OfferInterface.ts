45cffbb48e42d3d0fe4bdc5899040d90136f5b6b
kamil_pula <sorennx@gmail.com>
2023-12-17 20:49:22 +0100

    Updated OfferInterface

--- packages/shared/interfaces/offer/OfferInterface.ts
+++ packages/shared/interfaces/offer/OfferInterface.ts
@@ -3,5 +3,11 @@ import type { OfferProduct } from './OfferProduct'
 export interface Offer {
   id: number
   status: number
+  price: number
+  productQuantity: number
   product: OfferProduct
+  createdAt: string
+  updatedAt: string
+  startsAt: string
+  endsAt: string
 }
