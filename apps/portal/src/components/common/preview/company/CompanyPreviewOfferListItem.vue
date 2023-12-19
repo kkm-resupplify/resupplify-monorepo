4e0f321e34740075c018b836e2b0b92e69bc8a2a
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-19 16:25:22 +0100

    feat: add layout in list item

--- apps/portal/src/components/common/preview/company/CompanyPreviewOfferListItem.vue
+++ apps/portal/src/components/common/preview/company/CompanyPreviewOfferListItem.vue
@@ -1,7 +1,64 @@
-<template>CompanyPreviewOfferListItem</template>
+<template>
+  <a-list-item-wrapper class="company-preview-offer-list-item">
+    <a-image :src="offer.product.logoSrc" :width="120" :height="120" />
+
+    <div class="company-preview-offer-list-item__information">
+      <div class="company-preview-offer-list-item__product-information">
+        <a-title
+          :title="$t('preview.company.offer.product.name')"
+          :subtitle="offer.product.name"
+          variant="horizontal"
+          append-colon
+        />
+
+        <a-title
+          :title="$t('preview.company.offer.product.category')"
+          :subtitle="offer.product.productCategory.name"
+          variant="horizontal"
+          append-colon
+        />
+
+        <a-title
+          :title="$t('preview.company.offer.product.subcategory')"
+          :subtitle="offer.product.productSubcategory.name"
+          variant="horizontal"
+          append-colon
+        />
+
+        <product-tag-list :product-tags="offer.product.productTags" />
+      </div>
+
+      <div class="company-preview-offer-list-item__order-information">
+        <a-title
+          :title="$t('preview.company.offer.price')"
+          :subtitle="offer.product.netPrice"
+          variant="horizontal"
+          append-colon
+        />
+
+        <a-title
+          :title="$t('preview.company.offer.available')"
+          :subtitle="offer.product.quantity"
+          variant="horizontal"
+          append-colon
+        />
+
+        <a-title
+          :title="$t('preview.company.offer.ends')"
+          :subtitle="offer.endedAt"
+          variant="horizontal"
+          append-colon
+        />
+
+        <a-button :text="$t('global.addToCart')" size="x-large" />
+      </div>
+    </div>
+  </a-list-item-wrapper>
+</template>
 
 <script setup lang="ts">
 import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import ProductTagList from '../../product/ProductTagList.vue'
 import type { PropType } from 'vue'
 
 defineProps({
@@ -11,3 +68,27 @@ defineProps({
   }
 })
 </script>
+
+<style scoped lang="scss">
+.company-preview-offer-list-item {
+  &__information {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    white-space: nowrap;
+  }
+
+  &__product-information {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-30;
+    margin-left: $global-spacing-50;
+  }
+
+  &__order-information {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-30;
+  }
+}
+</style>
