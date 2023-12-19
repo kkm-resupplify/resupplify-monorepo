7ef55af050480356c135d4cede71b801e7e083bb
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-19 15:12:44 +0100

    feat: define structure of offer's list

---
+++ apps/portal/src/components/common/preview/company/CompanyPreviewOfferListItem.vue
@@ -0,0 +1,13 @@
+<template>CompanyPreviewOfferListItem</template>
+
+<script setup lang="ts">
+import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import type { PropType } from 'vue'
+
+defineProps({
+  offer: {
+    type: Object as PropType<Offer>,
+    required: true
+  }
+})
+</script>
