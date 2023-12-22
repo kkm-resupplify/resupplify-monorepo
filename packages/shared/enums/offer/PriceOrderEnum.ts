5570cf1ebcfe19046f9b050db97bf283485f10c2
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-22 23:57:07 +0100

    feat: add PriceOrderEnum

---
+++ packages/shared/enums/offer/PriceOrderEnum.ts
@@ -0,0 +1,6 @@
+import BaseEnum from '../BaseEnum'
+
+export default class PriceOrderEnum extends BaseEnum {
+  static readonly ASCENDING: string = 'asc'
+  static readonly DESCENDING: string = 'desc'
+}
