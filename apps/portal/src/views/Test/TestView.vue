487ba7608bc243354a2dbecfc53a82bc5e6f58ca
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-22 17:57:41 +0100

    fix: get rid of changes in TestView

--- apps/portal/src/views/Test/TestView.vue
+++ apps/portal/src/views/Test/TestView.vue
@@ -1,14 +1,11 @@
 <template>
   <basic-view-layout class="test-view">
-    <template #body>
-      <offer-filters />
-    </template>
+    <template #body> NIE UZYWAC CHYBA ZE NA SERIO NIE MA MIEJSCA </template>
   </basic-view-layout>
 </template>
 
 <script setup lang="ts">
 import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
-import OfferFilters from '@/components/common/offer/OfferFilters.vue'
 </script>
 
 <style lang="scss" scoped>
