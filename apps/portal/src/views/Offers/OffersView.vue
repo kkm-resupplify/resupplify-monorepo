86622b304a3b74c04d81b71e157195878efc6b0f
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-25 00:39:24 +0100

    Added API connection

--- apps/portal/src/views/Offers/OffersView.vue
+++ apps/portal/src/views/Offers/OffersView.vue
@@ -8,13 +8,17 @@
         </a-panel-section>
 
         <a-panel-section>
-          <offer-filters />
+          <offer-filters @filter="handleFetchOffers" />
 
           <a-line />
 
           <template v-if="isLoading">implement-loader-here</template>
 
-          <offer-list v-else :offers="offers" />
+          <template v-else>
+            <offer-list :offers="offers" />
+          </template>
+
+          <o-pagination :pagination="paginationData" @page-changed="handleFetchOffers" />
         </a-panel-section>
       </div>
     </template>
@@ -25,157 +29,47 @@
 import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
 import OfferFilters from '@/components/common/offer/OfferFilters.vue'
 import OfferList from '@/components/common/offer/OfferList.vue'
-import { ref } from 'vue'
+import { ref, onBeforeMount } from 'vue'
 import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import type { Pagination } from '@sharedInterfaces/config/PaginationInterface'
+import CompanyOffersService from '@/services/company/CompanyOffersService'
+import { useRoute } from 'vue-router'
 
 // Variables
-const offers = ref<Offer[]>([
-  {
-    id: 1,
-    price: 7,
-    productQuantity: 31,
-    warehouseQuantity: 122,
-    status: 1,
-    createdAt: '23-12-2023 01:31:02',
-    updatedAt: '23-12-2023 01:31:02',
-    startsAt: '23-12-2023 01:31:02',
-    endsAt: '25-12-2023 01:31:02',
-    product: {
-      id: 1,
-      name: 'Brazil coffee beans',
-      description: 'The best coffee beans from Brazil',
-      producer: 'Brazil Coffee Company',
-      code: 'coffee/grains',
-      status: 1,
-      verificationStatus: 1,
-      companyId: 1,
-      productUnit: {
-        id: 1,
-        code: 'KILOGRAM'
-      },
-      productCategory: {
-        id: 1,
-        name: 'Beverages'
-      },
-      productSubcategory: {
-        categoryId: 1,
-        id: 2,
-        name: 'Coffee and tea'
-      },
-      logoSrc: 'https://via.placeholder.com/120',
-      productTags: [],
-      translations: [
-        {
-          languageId: 1,
-          name: 'Brazil coffee beans',
-          description: 'The best coffee beans from Brazil'
-        },
-        {
-          languageId: 2,
-          name: 'Brazyilska kawa ziarnista',
-          description: 'Najlepsza kawa ziarnista z Brazylii'
-        }
-      ]
-    }
-  },
-  {
-    id: 5,
-    price: 6,
-    productQuantity: 15,
-    warehouseQuantity: 60,
-    status: 1,
-    createdAt: '23-12-2023 01:31:02',
-    updatedAt: '23-12-2023 01:31:02',
-    startsAt: '23-12-2023 01:31:02',
-    endsAt: '25-12-2023 01:31:02',
-    product: {
-      id: 1,
-      name: 'Brazil coffee beans',
-      description: 'The best coffee beans from Brazil',
-      producer: 'Brazil Coffee Company',
-      code: 'coffee/grains',
-      status: 1,
-      verificationStatus: 1,
-      companyId: 1,
-      productUnit: {
-        id: 1,
-        code: 'KILOGRAM'
-      },
-      productCategory: {
-        id: 1,
-        name: 'Beverages'
-      },
-      productSubcategory: {
-        categoryId: 1,
-        id: 2,
-        name: 'Coffee and tea'
-      },
-      logoSrc: 'https://via.placeholder.com/120',
-      productTags: [],
-      translations: [
-        {
-          languageId: 1,
-          name: 'Brazil coffee beans',
-          description: 'The best coffee beans from Brazil'
-        },
-        {
-          languageId: 2,
-          name: 'Brazyilska kawa ziarnista',
-          description: 'Najlepsza kawa ziarnista z Brazylii'
-        }
-      ]
-    }
-  },
-  {
-    id: 9,
-    price: 7,
-    productQuantity: 40,
-    warehouseQuantity: 160,
-    status: 1,
-    createdAt: '23-12-2023 01:31:02',
-    updatedAt: '23-12-2023 01:31:02',
-    startsAt: '23-12-2023 01:31:02',
-    endsAt: '25-12-2023 01:31:02',
-    product: {
-      id: 1,
-      name: 'Brazil coffee beans',
-      description: 'The best coffee beans from Brazil',
-      producer: 'Brazil Coffee Company',
-      code: 'coffee/grains',
-      status: 1,
-      verificationStatus: 1,
-      companyId: 1,
-      productUnit: {
-        id: 1,
-        code: 'KILOGRAM'
-      },
-      productCategory: {
-        id: 1,
-        name: 'Beverages'
-      },
-      productSubcategory: {
-        categoryId: 1,
-        id: 2,
-        name: 'Coffee and tea'
-      },
-      logoSrc: 'https://via.placeholder.com/120',
-      productTags: [],
-      translations: [
-        {
-          languageId: 1,
-          name: 'Brazil coffee beans',
-          description: 'The best coffee beans from Brazil'
-        },
-        {
-          languageId: 2,
-          name: 'Brazyilska kawa ziarnista',
-          description: 'Najlepsza kawa ziarnista z Brazylii'
-        }
-      ]
-    }
-  }
-])
 const isLoading = ref(false)
+const route = useRoute()
+const paginationData = ref<Pagination>()
+const offers = ref<Offer[]>([])
+
+// Methods
+const handleFetchOffers = async () => {
+  isLoading.value = true
+
+  const {
+    query: { page, name, categoryId, subcategoryId, status, price, dateEnd }
+  } = route
+  const { success, data, pagination } = await CompanyOffersService.getOffers({
+    page: page as string,
+    name: name as string,
+    categoryId: categoryId as string,
+    subcategoryId: subcategoryId as string,
+    status: status as string,
+    price: price as string,
+    dateEnd: dateEnd as string
+  })
+
+  if (success) {
+    offers.value = data
+    paginationData.value = pagination
+  }
+
+  isLoading.value = false
+}
+
+// Hooks
+onBeforeMount(() => {
+  handleFetchOffers()
+})
 </script>
 
 <style lang="scss" scoped>
