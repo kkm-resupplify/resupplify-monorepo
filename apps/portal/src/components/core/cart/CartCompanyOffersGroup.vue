5b2bf0a148973e53b01f2296a2ade4f3c7feb5a0
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-25 19:43:24 +0100

    Added group item placeholders

---
+++ apps/portal/src/components/core/cart/CartCompanyOffersGroup.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cart-company-offers-group">
+    <div>company-name</div>
+
+    <a-line />
+
+    <div>company-offer-item-list</div>
+
+    <div>footer</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+
+// Interfaces
+export interface CartCompanyGroup {
+  companyName: string
+  cartItems: CartItem[]
+}
+
+export interface CartItem {
+  offer: Offer
+  quantity: number
+}
+
+const props = defineProps({
+  groupData: {
+    type: Object as any
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.cart-company-offers-group {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ apps/portal/src/components/core/cart/CartContentSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-panel-section class="cart-content-section">
+    <a-title :title="$t('cart.main.content.title')" size="large" />
+
+    <a-list>
+      <cart-company-offers-group />
+    </a-list>
+  </a-panel-section>
+</template>
+
+<script setup lang="ts">
+import CartCompanyOffersGroup from '@/components/core/cart/CartCompanyOffersGroup.vue'
+</script>
