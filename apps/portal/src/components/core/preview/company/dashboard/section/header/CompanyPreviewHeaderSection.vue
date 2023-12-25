d805c850714cebff28330ec60daafd0773b9391f
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-25 16:56:30 +0100

    feat: add layout

--- apps/portal/src/components/core/preview/company/dashboard/section/header/CompanyPreviewHeaderSection.vue
+++ apps/portal/src/components/core/preview/company/dashboard/section/header/CompanyPreviewHeaderSection.vue
@@ -1,1 +1,38 @@
-<template>CompanyPreviewHeaderSection</template>
+<template>
+  <a-panel-section class="company-preview-header-section">
+    <div class="company-preview-header-section__wrapper">
+      <a-image
+        :width="120"
+        :height="120"
+        :alt="$t('common.offer.list.item.imageAlt')"
+        variant="rounded"
+      />
+      <div class="company-preview-header-section__info">
+        <a-title title="Company name" size="x-large" />
+
+        <a-title :title="$t('company.preview.email')" variant="horizontal" append-colon />
+
+        <a-title :title="$t('company.preview.phone')" variant="horizontal" append-colon />
+
+        <a-title :title="$t('company.preview.address')" variant="horizontal" append-colon />
+      </div>
+    </div>
+  </a-panel-section>
+</template>
+
+<style scoped lang="scss">
+.company-preview-header-section {
+  white-space: nowrap;
+
+  &__wrapper {
+    display: flex;
+    gap: $global-spacing-90;
+    padding: $global-spacing-30;
+  }
+
+  &__info {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
