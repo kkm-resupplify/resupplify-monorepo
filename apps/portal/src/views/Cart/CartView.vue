0d689c24244672e0bad30bb2c1c9075048279730
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-25 19:14:30 +0100

    Added initial CartView

---
+++ apps/portal/src/views/Cart/CartView.vue
@@ -0,0 +1,13 @@
+<template>
+  <basic-view-layout>
+    <template #body>
+      <div>cart-header</div>
+
+      <div>cart-company-group-list</div>
+    </template>
+  </basic-view-layout>
+</template>
+
+<script setup lang="ts">
+import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
+</script>
