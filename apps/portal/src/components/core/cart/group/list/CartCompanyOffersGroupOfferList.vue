207ab45ef9db51d0aaaac00aa06e87844f923a6c
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-26 00:41:41 +0100

    Added te,[;ates

---
+++ apps/portal/src/components/core/cart/group/list/CartCompanyOffersGroupOfferList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cart-company-offers-group-offer-list">
+    <!-- Your component content here -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CartCompanyOffersGroupOfferList'
+  // Your component options here
+})
+</script>
+
+<style lang="scss" scoped>
+.cart-company-offers-group-offer-list {
+  // Your component styles here
+}
+</style>
---
+++ apps/portal/src/components/core/cart/group/list/CartCompanyOffersGroupOfferListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cart-company-offers-group-offer-list-item">
+    <!-- Your component content here -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CartCompanyOffersGroupOfferListItem'
+  // Your component options here
+})
+</script>
+
+<style lang="scss" scoped>
+.cart-company-offers-group-offer-list-item {
+  /* Your component styles here */
+}
+</style>
