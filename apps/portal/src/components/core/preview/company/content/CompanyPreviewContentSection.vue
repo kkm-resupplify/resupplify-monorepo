6f0bf7ad2ace0edf475fbae9927c59c524d35372
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-26 20:10:08 +0100

    feat: place offer filters in layout

--- apps/portal/src/components/core/preview/company/content/CompanyPreviewContentSection.vue
+++ apps/portal/src/components/core/preview/company/content/CompanyPreviewContentSection.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>Offer filters</div>
+  <a-panel-section>
+    <offer-filters />
 
-  <div>Offer list</div>
+    <div>Offer list</div>
+  </a-panel-section>
 </template>
 
 <script setup lang="ts">
 import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import OfferFilters from '@/components/common/offer/OfferFilters.vue'
 import { ref } from 'vue'
 
 const offers = ref<Offer[]>([])
