7ff879145e07388c7551f689fbc71fa07a932f60
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-26 21:24:27 +0100

    feat: add getting offers by filter values

--- apps/portal/src/components/core/preview/company/content/CompanyPreviewContentSection.vue
+++ apps/portal/src/components/core/preview/company/content/CompanyPreviewContentSection.vue
@@ -1,15 +1,33 @@
 <template>
   <a-panel-section>
-    <offer-filters />
+    <offer-filters @filter="(filters) => handleFetchOffers(filters)" />
 
     <div>Offer list</div>
   </a-panel-section>
 </template>
 
 <script setup lang="ts">
-import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import type { Offer, OfferFiltersParams } from '@sharedInterfaces/offer/OfferInterface'
 import OfferFilters from '@/components/common/offer/OfferFilters.vue'
 import { ref } from 'vue'
+import router from '@/routes'
+import CompanyPreviewService from '@/services/preview/company/CompanyPreviewService'
 
+// Variables
 const offers = ref<Offer[]>([])
+const slug = router.currentRoute.value.params.slug as string
+const isLoading = ref(false)
+
+// Methods
+const handleFetchOffers = async (filters: OfferFiltersParams) => {
+  isLoading.value = true
+
+  const { data, success } = await CompanyPreviewService.getCompanyOffers(slug, filters)
+
+  if (success) {
+    offers.value = data
+  }
+
+  isLoading.value = false
+}
 </script>
