6e71eac8a9e8080c44e29172c146bc35ef862423
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-26 23:28:49 +0100

    Added orders prop

--- apps/portal/src/components/core/company/management/orders/list/OrderListItem.vue
+++ apps/portal/src/components/core/company/management/orders/list/OrderListItem.vue
@@ -1,8 +1,20 @@
 <template>
-  <a-list-item-wrapper class="order-list-item">order-list-item</a-list-item-wrapper>
+  <a-list-item-wrapper class="order-list-item">
+    {{ order }}
+  </a-list-item-wrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { type PropType } from 'vue'
+import type { Order } from '@sharedInterfaces/order/OrderInterface'
+
+defineProps({
+  order: {
+    type: Object as PropType<Order>,
+    required: true
+  }
+})
+</script>
 
 <style lang="scss" scoped>
 .order-list-item {
