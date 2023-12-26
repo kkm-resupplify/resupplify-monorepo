eac29a135ba4e1490dbb2591abb255a6082e1433
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-27 00:00:32 +0100

    fix: add missing import and variable in CompanyPreviewService

--- apps/portal/src/services/preview/company/CompanyPreviewService.ts
+++ apps/portal/src/services/preview/company/CompanyPreviewService.ts
@@ -1,6 +1,9 @@
 import BaseService from '@/services/BaseService'
+import type { OfferFiltersParams } from '@sharedInterfaces/offer/OfferInterface'
 
 class CompanyPreviewService extends BaseService {
+  static COMPANY_PREVIEW_OFFERS = 'productOffer/company'
+  
   async getCompanyInformation(id: string | number) {
     return await this.get({ id })
   }
