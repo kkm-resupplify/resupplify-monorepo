2a903015729682bfa2dd9e0bf6410d9a9a92f3ec
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-26 23:49:04 +0100

    feat: add list to layout

--- apps/portal/src/components/core/preview/company/content/CompanyPreviewContentSection.vue
+++ apps/portal/src/components/core/preview/company/content/CompanyPreviewContentSection.vue
@@ -1,12 +1,40 @@
 <template>
   <div>Offer filters</div>
 
-  <div>Offer list</div>
+  <template v-if="isLoading">implement-loader-here</template>
+
+  <template v-else>
+    <offer-list v-if="showList" :offers="offers" />
+
+    <a-list-no-results v-else :text="$t(`common.offer.list.${noResultsTranslationKey}`)" />
+  </template>
 </template>
 
 <script setup lang="ts">
 import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import { computed } from 'vue'
 import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 
+// Variables
+const isLoading = ref(false)
 const offers = ref<Offer[]>([])
+const route = useRoute()
+
+// Computed
+const showList = computed(() => {
+  return offers.value.length > 0
+})
+
+const filtersUsed = computed(() => {
+  const {
+    query: { page, name, categoryId, subcategoryId, status, price, dateEnd }
+  } = route
+
+  return !!(page !== '1' || (name ?? categoryId ?? subcategoryId ?? status ?? price ?? dateEnd))
+})
+
+const noResultsTranslationKey = computed(() => {
+  return offers.value.length === 0 && filtersUsed.value ? 'noOffersMatchingFilter' : 'noOffers'
+})
 </script>
