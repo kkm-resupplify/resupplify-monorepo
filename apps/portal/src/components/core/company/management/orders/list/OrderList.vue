2109bf8a8a8ad28fc3b07b6508ea863b47a0ab13
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-26 22:36:39 +0100

    Added intiial OrderList and OrderListItem

---
+++ apps/portal/src/components/core/company/management/orders/list/OrderList.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-list>
+    <order-list-item />
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import OrderListItem from '@/components/core/company/management/orders/list/OrderListItem.vue'
+</script>
