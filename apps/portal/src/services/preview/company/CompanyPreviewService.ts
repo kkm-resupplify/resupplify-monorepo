a66f27292530a49e608407ac1fc36f1c0b49c892
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-26 23:59:06 +0100

    feat: add method for getting offers from company in CompanyPreviewService

--- apps/portal/src/services/preview/company/CompanyPreviewService.ts
+++ apps/portal/src/services/preview/company/CompanyPreviewService.ts
@@ -4,6 +4,13 @@ class CompanyPreviewService extends BaseService {
   async getCompanyInformation(id: string | number) {
     return await this.get({ id })
   }
+
+  async getCompanyOffers(id: string | number, params?: OfferFiltersParams) {
+    return await this.get({
+      suffix: `${CompanyPreviewService.COMPANY_PREVIEW_OFFERS}/${id}`,
+      config: { params }
+    })
+  }
 }
 
 export default new CompanyPreviewService('/preview/company')
