1d5e2be8ef085c14e314f133d3bd8eb304ca8399
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-28 00:29:48 +0100

    Added border seperating order items

--- apps/portal/src/components/core/company/management/orders/list/OrderItemList.vue
+++ apps/portal/src/components/core/company/management/orders/list/OrderItemList.vue
@@ -1,9 +1,10 @@
 <template>
-  <a-list>
+  <a-list class="order-item-list">
     <order-item-list-item
-      v-for="orderItem in orderItems"
+      v-for="(orderItem, idx) in orderItems"
       :key="orderItem.id"
       :order-item="orderItem"
+      :style="borderStyle(idx)"
     />
   </a-list>
 </template>
@@ -13,10 +14,23 @@ import { type PropType } from 'vue'
 import type { OrderItem } from '@sharedInterfaces/order/OrderInterface'
 import OrderItemListItem from '@/components/core/company/management/orders/list/OrderItemListItem.vue'
 
-defineProps({
+const props = defineProps({
   orderItems: {
     type: Array as PropType<OrderItem[]>,
     required: true
   }
 })
+
+// Methods
+const borderStyle = (idx: number) => {
+  return {
+    borderBottom: idx === props.orderItems.length - 1 ? 'none' : '2px solid var(--secondary-2)'
+  }
+}
 </script>
+
+<style scoped lang="scss">
+.order-item-list {
+  flex: 1;
+}
+</style>
