995c5351e1c8959d2ced2573c5ec6ee1ec2d578d
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-27 23:39:59 +0100

    Added initial OrderItemListItem

---
+++ apps/portal/src/components/core/company/management/orders/list/OrderItemListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ orderItem }}</div>
+</template>
+
+<script setup lang="ts">
+import { type PropType } from 'vue'
+import type { OrderItem } from '@sharedInterfaces/order/OrderInterface'
+
+defineProps({
+  orderItem: {
+    type: Object as PropType<OrderItem>,
+    required: true
+  }
+})
+</script>
