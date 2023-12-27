f7601b6e1ba8ad85ed684c227114b49eddbf1f0e
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-28 00:45:34 +0100

    feat: add fetching offer

--- apps/portal/src/components/core/preview/offer/header/OfferPreviewHeaderSection.vue
+++ apps/portal/src/components/core/preview/offer/header/OfferPreviewHeaderSection.vue
@@ -1,1 +1,32 @@
 <template>offer-preview-header-section</template>
+
+<script setup lang="ts">
+import OfferPreviewService from '@/services/preview/offer/OfferPreviewService'
+import type { Offer } from '@sharedInterfaces/offer/OfferInterface'
+import { computed, onBeforeMount } from 'vue'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+// Variables
+const offer = ref<Offer>()
+const route = useRoute()
+
+// Computed
+const id = computed(() => {
+  return route.params.id as string
+})
+
+// Methods
+const handleFetchOffer = async () => {
+  const { data, success } = await OfferPreviewService.getOffer(id.value)
+
+  if (success) {
+    console.log(data)
+  }
+}
+
+// Hooks
+onBeforeMount(() => {
+  handleFetchOffer()
+})
+</script>
