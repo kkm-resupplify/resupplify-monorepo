e691f801305dbe4b917eb65adce3f1bad1bc7266
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-27 18:25:28 +0100

    feat: add layout

---
+++ apps/portal/src/components/core/companies/CompaniesFilters.vue
@@ -0,0 +1,43 @@
+<template>
+  <o-form class="companies-filters">
+    <template #body>
+      <div class="companies-filters__inputs">
+        <m-text-field
+          name="name"
+          :label="$t('companies.filters.nameLabel')"
+          :placeholder="$t('companies.filters.namePlaceholder')"
+          :validate="false"
+        />
+
+        <m-select
+          name="categoryId"
+          :label="$t('companies.filters.categoryLabel')"
+          :placeholder="$t('companies.filters.categoryPlaceholder')"
+          :validate="false"
+        />
+      </div>
+    </template>
+
+    <template #footer>
+      <div class="companies-filters__buttons">
+        <a-button button-type="submit" :text="$t('global.search')" size="x-large" />
+
+        <a-button :text="$t('global.reset')" size="x-large" />
+      </div>
+    </template>
+  </o-form>
+</template>
+
+<style scoped lang="scss">
+.companies-filters {
+  &__inputs {
+    display: flex;
+    gap: $global-spacing-30;
+  }
+
+  &__buttons {
+    display: flex;
+    gap: $global-spacing-30;
+  }
+}
+</style>
--- apps/portal/src/views/Companies/CompaniesView.vue
+++ apps/portal/src/views/Companies/CompaniesView.vue
@@ -1,19 +1,36 @@
 <template>
   <basic-view-layout>
     <template #body>
-      <a-panel-section>
-        <a-title :title="$t('companies.header.title')" size="x-large" />
+      <div class="companies-view__content">
+        <a-panel-section>
+          <a-title :title="$t('companies.header.title')" size="x-large" />
 
-        <a-title :title="$t('companies.header.description')" />
-      </a-panel-section>
+          <a-title :title="$t('companies.header.description')" />
+        </a-panel-section>
 
-      <div>companies-filters</div>
+        <a-panel-section>
+          <companies-filters />
 
-      <div>companies-list</div>
+          <a-line />
+
+          <div>companies-list</div>
+        </a-panel-section>
+      </div>
     </template>
   </basic-view-layout>
 </template>
 
 <script setup lang="ts">
+import CompaniesFilters from '@/components/core/companies/CompaniesFilters.vue'
 import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
 </script>
+
+<style lang="scss" scoped>
+.companies-view {
+  &__content {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-20;
+  }
+}
+</style>
