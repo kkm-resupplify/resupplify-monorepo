56080b9b10156d19cf3f460e1bc82bacc9031b1b
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-27 23:40:14 +0100

    Added OrderItemList

---
+++ apps/portal/src/components/core/company/management/orders/list/OrderItemList.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-list>
+    <order-item-list-item
+      v-for="orderItem in orderItems"
+      :key="orderItem.id"
+      :order-item="orderItem"
+    />
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import { type PropType } from 'vue'
+import type { OrderItem } from '@sharedInterfaces/order/OrderInterface'
+import OrderItemListItem from '@/components/core/company/management/orders/list/OrderItemListItem.vue'
+
+defineProps({
+  orderItems: {
+    type: Array as PropType<OrderItem[]>,
+    required: true
+  }
+})
+</script>
