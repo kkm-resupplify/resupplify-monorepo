14ac9d1f187933fc512bb5007e501fd75d169f82
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-27 22:19:26 +0100

    feat: add CompaniesList to CompaniesView

--- apps/portal/src/views/Companies/CompaniesView.vue
+++ apps/portal/src/views/Companies/CompaniesView.vue
@@ -9,11 +9,57 @@
 
       <div>companies-filters</div>
 
-      <div>companies-list</div>
+      <companies-list :companies="companies" />
     </template>
   </basic-view-layout>
 </template>
 
 <script setup lang="ts">
 import BasicViewLayout from '@/layouts/view/BasicViewLayout.vue'
+import CompaniesList from '@/components/core/companies/list/CompaniesList.vue'
+
+const companies = [
+  {
+    id: 1,
+    name: 'Coffe Company',
+    shortDescription: 'Coffe Company with 10 years of experience in coffee market',
+    description: 'Coffe Company with 10 years of experience in coffee market',
+    slug: 'coffe-company',
+    ownerId: 1,
+    status: 0,
+    details: {
+      countryId: 42,
+      address: 'Al. Niepodległości 61 Szczecin',
+      email: 'coofeCompany@gmail.com',
+      phoneNumber: '+48604212365',
+      externalWebsite: 'https://coofeCompany.com',
+      logo: 'logo.png',
+      companyId: 1,
+      companyCategoryId: 1,
+      tin: '1159015689',
+      contactPerson: 'Maciej Pokręć'
+    }
+  },
+  {
+    id: 3,
+    name: 'Java Coffee',
+    shortDescription: 'This is Java Coffee',
+    description: 'This is Java Coffee selling coffee',
+    slug: 'java-coffee',
+    ownerId: 3,
+    status: 1,
+    details: {
+      countryId: 12,
+      address: 'Helsinki, Finland',
+      email: 'javacoffee@gmail.com',
+      phoneNumber: '+48123456789',
+      externalWebsite: 'https://www.javacoffee.com',
+      logo: 'xdd',
+      companyId: 3,
+      companyCategoryId: 1,
+      tin: '1234',
+      contactPerson: 'Olivia Anderson'
+    }
+  }
+]
 </script>
