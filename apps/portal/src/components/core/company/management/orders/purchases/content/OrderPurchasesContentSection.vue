fbc5ab892fbe62675a616ebd6eea955561b9e3f2
Kamil Pula <kamil.pula.dev@gmail.com>
2023-12-30 17:58:25 +0100

    Fixed translation key

--- apps/portal/src/components/core/company/management/orders/purchases/content/OrderPurchasesContentSection.vue
+++ apps/portal/src/components/core/company/management/orders/purchases/content/OrderPurchasesContentSection.vue
@@ -50,7 +50,7 @@ const filtersUsed = computed(() => {
 
 const noResultsTranslationKey = computed(() => {
   return orders.value.length === 0 && filtersUsed.value
-    ? 'company.management.order.noOrdersMatchingFilter'
+    ? 'noOrdersMatchingFilter'
     : 'noOrdersBought'
 })
 
