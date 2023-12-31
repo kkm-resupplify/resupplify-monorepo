578180c18588dd6c78afdbe39362d730ec420fb8
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2024-01-01 00:10:09 +0100

    fix: add necessary suffixes in CompanyOffersService with polish translations for notifications

--- apps/portal/src/services/company/CompanyOffersService.ts
+++ apps/portal/src/services/company/CompanyOffersService.ts
@@ -4,6 +4,8 @@ import type { OfferFiltersParams, CreateOffer } from '@sharedInterfaces/offer/Of
 class CompanyOfferService extends BaseService {
   static STOCK_ITEMS_SUFFIX = 'stockItems'
   static COMPANY_OFFERS_SUFFIX = 'companyOffers'
+  static DEACTIVATE_OFFER_SUFFIX = 'deactivateOffer'
+  static ACTIVATE_OFFER_SUFFIX = 'activateOffer'
 
   async getOffers(params?: OfferFiltersParams) {
     return await this.get({
@@ -33,14 +35,14 @@ class CompanyOfferService extends BaseService {
 
   async deactivateOffer(id: number) {
     return await this.put({
-      id: id,
+      suffix: `${CompanyOfferService.DEACTIVATE_OFFER_SUFFIX}/${id}`,
       notificationTitle: 'company.management.offer.creator.notification.offerWithdrawalSuccessTitle'
     })
   }
 
   async activateOffer(id: number) {
     return await this.put({
-      id: id,
+      suffix: `${CompanyOfferService.ACTIVATE_OFFER_SUFFIX}/${id}`,
       notificationTitle: 'company.management.offer.creator.notification.offerActivationSuccessTitle'
     })
   }
