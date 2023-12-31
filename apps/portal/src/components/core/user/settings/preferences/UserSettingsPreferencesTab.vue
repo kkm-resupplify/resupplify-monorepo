52c8626a17f8eae40e7968645ba69eae7e9e3b5a
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-31 18:06:54 +0100

    feat: add layout

--- apps/portal/src/components/core/user/settings/preferences/UserSettingsPreferencesTab.vue
+++ apps/portal/src/components/core/user/settings/preferences/UserSettingsPreferencesTab.vue
@@ -1,3 +1,53 @@
 <template>
-    user-settings-preferences-tab
+  <div class="user-settings-preferences-tab">
+    <a-title :title="$t('settings.preferences.title')" size="xx-large" />
+
+    <o-form>
+      <template #body>
+        <div class="user-settings-preferences-tab__content">
+          <m-select
+            name="language"
+            :label="$t('settings.preferences.setLanguageLabel')"
+            :placeholder="$t('settings.preferences.setLanguagePlaceholder')"
+            rules="required"
+          />
+
+          <m-select
+            name="language"
+            :label="$t('settings.preferences.setThemeLabel')"
+            :placeholder="$t('settings.preferences.setThemePlaceholder')"
+            rules="required"
+          />
+        </div>
+      </template>
+
+      <template #footer>
+        <a-button
+          button-type="submit"
+          :text="$t('settings.preferences.saveChanges')"
+          size="x-large"
+          class="user-settings-preferences-tab__button"
+        />
+      </template>
+    </o-form>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.user-settings-preferences-tab {
+  display: flex;
+  flex-direction: column;
+  gap: $global-spacing-50;
+  max-height: 260px;
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-50;
+  }
+
+  &__button {
+    width: 100%;
+  }
+}
+</style>
