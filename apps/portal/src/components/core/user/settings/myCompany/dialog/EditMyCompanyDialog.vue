b8ae25b82d8114fad2ac96b3207ae62bc6034562
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2023-12-31 20:59:16 +0100

    feat: add modal with company editor with values from store

--- apps/portal/src/components/core/user/settings/myCompany/dialog/EditMyCompanyDialog.vue
+++ apps/portal/src/components/core/user/settings/myCompany/dialog/EditMyCompanyDialog.vue
@@ -1,5 +1,10 @@
 <template>
-  <m-dialog ref="dialogRef" :title="company.name">
+  <m-dialog
+    ref="dialogRef"
+    :title="company.name"
+    @open="setCompanyEditorData"
+    @close="companyRegisterStore.$reset()"
+  >
     <template #activator>
       <a-button
         :text="$t('settings.myCompany.editDetails')"
@@ -16,6 +21,9 @@
 import type { CompanyData } from '@sharedInterfaces/company/CompanyInterface'
 import { type PropType } from 'vue'
 import RegisterCompany from '@/components/core/company/register/RegisterCompany.vue'
+import { computed, ref } from 'vue'
+import { useRegisterCompanyStore } from '@/stores/company/useRegisterCompanyStore'
+import type MDialogVue from '@sharedMolecules/dialog/MDialog.vue'
 
 const props = defineProps({
   company: {
@@ -23,4 +31,44 @@ const props = defineProps({
     required: true
   }
 })
+
+// Variables
+const dialogRef = ref<null | InstanceType<typeof MDialogVue>>(null)
+const companyRegisterStore = useRegisterCompanyStore()
+
+// Computed
+const editCompanyGeneralStepData = computed(() => {
+  return {
+    id: props.company.id,
+    name: props.company.name,
+    countryId: props.company.details.countryId,
+    shortDescription: props.company.shortDescription,
+    description: props.company.description
+  }
+})
+
+const editCompanyDetailsStep = computed(() => {
+  return {
+    address: props.company.details.address,
+    companyCategoryId: props.company.details.companyCategoryId,
+    tin: props.company.details.tin,
+    logo: props.company.details.logo
+  }
+})
+
+const editCompanyContactInfoStep = computed(() => {
+  return {
+    phoneNumber: props.company.details.phoneNumber,
+    email: props.company.details.email,
+    contactPerson: props.company.details.contactPerson,
+    externalWebsite: props.company.details.externalWebsite
+  }
+})
+
+// Methods
+const setCompanyEditorData = () => {
+  companyRegisterStore.setGeneralStepData(editCompanyGeneralStepData.value)
+  companyRegisterStore.setDetailsStepData(editCompanyDetailsStep.value)
+  companyRegisterStore.setContactInfoStepData(editCompanyContactInfoStep.value)
+}
 </script>
