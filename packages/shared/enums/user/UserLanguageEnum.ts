31fe3c76e09162aebf799f27b03a260282d0b2e1
Kamil Pula <kamil.pula.dev@gmail.com>
2024-01-01 19:23:14 +0100

    Restructured UserLanguageEnum

--- packages/shared/enums/user/UserLanguageEnum.ts
+++ packages/shared/enums/user/UserLanguageEnum.ts
@@ -1,6 +1,14 @@
 import BaseEnum from '@sharedEnums/BaseEnum'
+interface Language {
+  id: number
+  key: string
+}
 
 export default class UserLanguageEnum extends BaseEnum {
-  static readonly POLISH: string = 'pl-PL'
-  static readonly ENGLISH: string = 'en-US'
+  static readonly ENGLISH: Language = { id: 1, key: 'en-US' }
+  static readonly POLISH: Language = { id: 2, key: 'pl-PL' }
+
+  static getLanguageById(id: number): Language {
+    return this.getAllValues().find((value) => value.id === id)
+  }
 }
