f6ab56eb10f758f7792975fc7b3c919f0518c709
karolrak50@gmail.com <83764437+krl8456@users.noreply.github.com>
2024-01-02 22:30:49 +0100

    feat: add more information to company section

--- apps/portal/src/components/core/preview/offer/content/OfferPreviewContentSection.vue
+++ apps/portal/src/components/core/preview/offer/content/OfferPreviewContentSection.vue
@@ -17,37 +17,85 @@
         variant="rounded"
       />
 
-      <div class="offer-preview-content-section__company-information">
-        <a-link
-          :text="company.name"
-          :link="{ name: RouteNames.COMPANY_PREVIEW, params: { slug: company.slug } }"
-          size="large"
-        />
+      <div class="offer-preview-content-section__company-data">
+        <div class="offer-preview-content-section__company-data-left-section">
+          <a-link
+            :text="company.name"
+            :link="{ name: RouteNames.COMPANY_PREVIEW, params: { slug: company.slug } }"
+            size="x-large"
+          />
 
-        <a-title
-          :title="$t('company.preview.email')"
-          :subtitle="company.details.email"
-          variant="horizontal"
-          append-colon
-          class="offer-preview-content-section__title"
-        />
+          <a-title
+            :title="$t('company.preview.email')"
+            :subtitle="company.details.email"
+            variant="horizontal"
+            append-colon
+            class="offer-preview-content-section__title"
+            size="large"
+          />
 
-        <a-title
-          :title="$t('company.preview.phone')"
-          :subtitle="company.details.phoneNumber"
-          variant="horizontal"
-          append-colon
-          class="offer-preview-content-section__title"
-        />
+          <a-title
+            :title="$t('company.preview.phone')"
+            :subtitle="company.details.phoneNumber"
+            variant="horizontal"
+            append-colon
+            class="offer-preview-content-section__title"
+            size="large"
+          />
 
-        <a-title
-          :title="$t('company.preview.address')"
-          :subtitle="company.details.address"
-          variant="horizontal"
-          append-colon
-          class="offer-preview-content-section__title"
-        />
+          <a-title
+            :title="$t('company.preview.address')"
+            :subtitle="company.details.address"
+            variant="horizontal"
+            append-colon
+            class="offer-preview-content-section__title"
+            size="large"
+          />
+        </div>
+
+        <div class="offer-preview-content-section__company-data-right-section">
+          <a-title
+            :title="$t('company.preview.contactPerson')"
+            :subtitle="company.details.contactPerson"
+            variant="horizontal"
+            append-colon
+            class="offer-preview-content-section__title"
+            size="large"
+          />
+
+          <a-title
+            :title="$t('company.preview.tin')"
+            :subtitle="company.details.tin"
+            variant="horizontal"
+            append-colon
+            class="offer-preview-content-section__title"
+            size="large"
+          />
+
+          <a-title
+            :title="$t('company.preview.externalWebsite')"
+            :subtitle="company.details.externalWebsite"
+            variant="horizontal"
+            append-colon
+            class="offer-preview-content-section__title"
+            size="large"
+          />
+        </div>
       </div>
+    </div>
+
+    <div class="offer-preview-content-section__company-description">
+      <a-title
+        :title="$t('company.preview.shortDescription')"
+        :subtitle="company.shortDescription"
+        size="large"
+      />
+
+      <a-title
+        :title="$t('company.preview.longDescription')"
+        :subtitle="company.description"
+        size="large"
+      />
     </div>
   </a-panel-section>
 </template>
@@ -84,7 +132,15 @@ defineProps({
     white-space: nowrap;
   }
 
-  &__company-information {
+  &__company-data {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  &__company-data-left-section,
+  &__company-data-right-section {
     display: flex;
     flex-direction: column;
     gap: $global-spacing-20;
@@ -92,6 +148,13 @@ defineProps({
 
   &__company-link {
     text-decoration: none;
+  }
+
+  &__company-description {
+    display: flex;
+    flex-direction: column;
+    gap: $global-spacing-50;
+    margin-top: $global-spacing-100;
   }
 }
 </style>
