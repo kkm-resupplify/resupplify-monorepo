58b3ac577d0ca0997cdbe28d289dcf4ee074e82d
Kamil Pula <kamil.pula.dev@gmail.com>
2024-01-03 01:52:23 +0100

    Updated error notification logic

--- apps/portal/src/services/HttpErrorHandler.ts
+++ apps/portal/src/services/HttpErrorHandler.ts
@@ -63,6 +63,8 @@ class HttpErrorHandler {
 
   clearNotificationStore() {
     const generalNotificationStore = useGeneralNotificationStore()
+    // Display current error notification and clear the rest
+    generalNotificationStore.displayNextNotification()
     generalNotificationStore.clearNotifications()
   }
 
